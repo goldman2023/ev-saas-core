6a7a517661deb646d944d55bf91ef86a5c310962
Vukasin Jockovic <jockovicvukasin@gmail.com>
2021-08-11 17:58:38 +0200

    PGSQL + Octane + Redis + Kool + Docker support - init version 1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,6 +21,26 @@ class Kernel extends ConsoleKernel
         \Stancl\Tenancy\Commands\Migrate::class,
         \Stancl\Tenancy\Commands\Seed::class,
         \Qirolab\Theme\Commands\MakeThemeCommand::class,
+
+        // For some very weird reason some commands from few packages are not added to artisan even though there is no error in code nor in logs
+        // They also seem properly resolved through package ServiceProviders, but still...they are not visible when running: php artisan list
+        // Examples: php artisan octane:{start:install:stop...etc} or php artisan sail:{install, publish} are returning following error pattern:
+        // "There are no commands defined in the octane/sail namespace"
+        // This should work out of the box since we are using laravel 8+...I don't know what's the problem tbh
+        // I'm adding these commands manually here because they are not added to artisan for whatever reason...
+
+        // Octane
+        \Laravel\Octane\Commands\InstallCommand::class,
+        \Laravel\Octane\Commands\ReloadCommand::class,
+        \Laravel\Octane\Commands\StartCommand::class,
+        \Laravel\Octane\Commands\StartRoadRunnerCommand::class,
+        \Laravel\Octane\Commands\StartSwooleCommand::class,
+        \Laravel\Octane\Commands\StatusCommand::class,
+        \Laravel\Octane\Commands\StopCommand::class,
+
+        // Sail
+        \Laravel\Sail\Console\InstallCommand::class,
+        \Laravel\Sail\Console\PublishCommand::class,
     ];
 
     /**
