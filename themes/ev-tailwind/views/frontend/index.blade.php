b989342d0470b461cafb72cc7af57dd83f91cf73
Eimantas <eim.kasp@gmail.com>
2021-08-13 21:13:20 +0300

    initial category component

--- themes/ev-tailwind/views/frontend/index.blade.php
+++ themes/ev-tailwind/views/frontend/index.blade.php
@@ -1,7 +1,9 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-    <x-tenant.hero.simple-centered button="Labas"></x-tenant.hero.simple-centered>
+    <x-tenant.hero.simple-centered></x-tenant.hero.simple-centered>
 
     <x-tenant.product-list></x-tenant.product-list>
+
+    <x-tenant.category.previews.with-scrolling-cards></x-tenant.category.previews.with-scrolling-cards>
 @endsection
