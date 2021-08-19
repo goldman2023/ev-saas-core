83b556bf59694fa0e0936ff9684951fc32b8dbfd
kangjin <sws092298@outlook.com>
2021-08-19 21:52:06 +0800

    fix the no reviews exception

--- app/View/Components/Tenant/Product/reviews/ResultReviews.php
+++ app/View/Components/Tenant/Product/reviews/ResultReviews.php
@@ -9,7 +9,7 @@ class ResultReviews extends Component
     public $reviews;
     public $total_rating = 0;
     public $average_rating = 0;
-    public $count = 0 ;
+    public $count = 0;
     public $each_stars = array();
     /**
      * Create a new component instance.
@@ -47,13 +47,20 @@ class ResultReviews extends Component
             }
         }
 
-        $this->each_stars[5] = round(($five_count/$this->count) * 100); 
-        $this->each_stars[4] = round(($four_count/$this->count) * 100); 
-        $this->each_stars[3] = round(($three_count/$this->count) * 100); 
-        $this->each_stars[2] = round(($two_count/$this->count) * 100); 
-        $this->each_stars[1] = round(($one_count/$this->count) * 100); 
-        $this->average_rating = round($this->total_rating/$this->count);
-
+        if ($this->count > 0) {
+            $this->each_stars[5] = round(($five_count / $this->count) * 100);
+            $this->each_stars[4] = round(($four_count / $this->count) * 100);
+            $this->each_stars[3] = round(($three_count / $this->count) * 100);
+            $this->each_stars[2] = round(($two_count / $this->count) * 100);
+            $this->each_stars[1] = round(($one_count / $this->count) * 100);
+            $this->average_rating = round($this->total_rating / $this->count);
+        }else {
+            for($i = 1; $i< 6 ; $i ++){
+                $this->each_stars[$i] = 0;
+            }
+            $this->average_rating = 0;
+        }
+        
     }
 
     /**
--- resources/views/components/tenant/product/reviews/show-reviews.blade.php
+++ resources/views/components/tenant/product/reviews/show-reviews.blade.php
@@ -3,15 +3,19 @@
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:py-32 lg:px-8 lg:grid lg:grid-cols-12 lg:gap-x-8">
         <!-- Review Result Section  -->
 
-      
+
         <x-tenant.product.reviews.result-reviews :reviews="$product->reviews"></x-tenant.product.reviews.result-reviews>
         <!-- End Review Result Section  -->
 
         <div class="mt-16 lg:mt-0 lg:col-start-6 lg:col-span-7">
             <h3 class="sr-only">Recent reviews</h3>
-            @foreach($product->reviews as $key => $review)
-                <x-tenant.product.reviews.review-card :review="$review "></x-tenant.product.reviews.review-card>
-            @endforeach
+            @if(count($product->reviews)>0)
+                @foreach($product->reviews as $key => $review)
+                    <x-tenant.product.reviews.review-card :review="$review "></x-tenant.product.reviews.review-card>
+                @endforeach
+            @else
+                <p class="mt-5">No reviews</p>
+            @endif
         </div>
     </div>
     <x-tenant.product.reviews.add-review-modal :product="$product"></x-tenant.product.reviews.add-review-modal>
