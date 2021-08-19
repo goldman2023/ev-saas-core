d6fc803749ff036a9f9e2deb87b0f0cdb2e52ad4
Eimantas <eim.kasp@gmail.com>
2021-08-19 19:20:21 +0300

    dynamic labels initial version
    Right now it does not take into consideration the multilanguage and the component it's comming, from ,but all the data can be found in "ev_labels" table

---
+++ database/migrations/tenant/2021_08_19_190245_create_ev_labels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEvLabelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ev_labels', function (Blueprint $table) {
+            $table->id();
+            $table->string('key');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ev_labels');
+    }
+}
