6369c5174570bedc3ff1643b37d07b32b90aa1cb
kangjin <sws092298@outlook.com>
2021-08-20 17:14:34 +0800

    change open modal with alpine js from javascript

--- resources/views/components/tenant/product/reviews/add-review-modal.blade.php
+++ resources/views/components/tenant/product/reviews/add-review-modal.blade.php
@@ -1,4 +1,4 @@
-<div id="interestModal" class="fixed z-10 inset-0 invisible overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+<div id="interestModal" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
         <!-- This element is to trick the browser into centering the modal contents. -->
@@ -36,7 +36,7 @@
                         <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm">
                             Send
                         </button>
-                        <button type="button" onclick="closeModal()" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm">
+                        <button type="button" @click="open = false" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm">
                             Cancel
                         </button>
                     </div>
--- resources/views/components/tenant/product/reviews/show-reviews.blade.php
+++ resources/views/components/tenant/product/reviews/show-reviews.blade.php
@@ -1,5 +1,5 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-white">
+<div class="bg-white" x-data="{ 'open': false }" @keydown.escape="open = false">
     <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:py-32 lg:px-8 lg:grid lg:grid-cols-12 lg:gap-x-8">
         <!-- Review Result Section  -->
 
@@ -10,25 +10,16 @@
         <div class="mt-16 lg:mt-0 lg:col-start-6 lg:col-span-7">
             <h3 class="sr-only">Recent reviews</h3>
             @if(count($product->reviews)>0)
-                @foreach($product->reviews as $key => $review)
-                    <x-tenant.product.reviews.review-card :review="$review "></x-tenant.product.reviews.review-card>
-                @endforeach
+            @foreach($product->reviews as $key => $review)
+            <x-tenant.product.reviews.review-card :review="$review "></x-tenant.product.reviews.review-card>
+            @endforeach
             @else
-                <p class="mt-5">No reviews</p>
+            <p class="mt-5">No reviews</p>
             @endif
         </div>
     </div>
-    <x-tenant.product.reviews.add-review-modal :product="$product"></x-tenant.product.reviews.add-review-modal>
+
+    <div x-show="open">
+        <x-tenant.product.reviews.add-review-modal :product="$product"></x-tenant.product.reviews.add-review-modal>
+    </div>
 </div>
-
-@section('script')
-<script type="text/javascript">
-    function openModal() {
-        document.getElementById('interestModal').classList.remove("invisible");
-    }
-
-    function closeModal() {
-        document.getElementById('interestModal').classList.add("invisible");
-    }
-</script>
-@endsection
