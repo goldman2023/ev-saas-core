0ce92beb211483e885c5e9e0316a901a9a514260
Eimantas <eim.kasp@gmail.com>
2021-08-21 03:22:35 +0300

    inline editing style updates

--- resources/views/livewire/dynamic-label.blade.php
+++ resources/views/livewire/dynamic-label.blade.php
@@ -8,17 +8,30 @@
     </button>
 
     @if ($show_input_field)
-    {{-- TODO: Improve UX and UI of this element --}}
-        <div class="absolute top-0">
+        {{-- TODO: Improve UX and UI of this element --}}
+        <div class="absolute top-0 bg-white p-3 rounded-md shadow-xl z-90" style="z-index: 99999">
 
-            <input type="text" wire:model="label.value" class="mt-2 text-sm sm:text-base pl-2 pr-4
-        rounded-lg border border-gray-400 w-full py-2
-        focus:outline-none focus:border-blue-400" />
 
-            <button wire:click.prevent="updateLabel()">
-                {{ translate('Save') }}
-            </button>
+            <label for="email" class="block text-sm font-medium text-gray-700">
+                {{ translate('Edit Label') }}
+            </label>
+            <div class="mt-1 flex rounded-md shadow-sm">
+                <div class="relative flex items-stretch flex-grow focus-within:z-10">
 
+                    <input wire:model="label.value" type="text" name="email" id="email"
+                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md sm:text-sm border-gray-300"
+                        placeholder="John Doe">
+                </div>
+                <button type="button"
+                wire:click.prevent="updateLabel()"
+                    class="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                    <!-- Heroicon name: solid/sort-ascending -->
+
+                    <span>{{ translate('Save')}} </span>
+                </button>
+            </div>
         </div>
+
+
     @endif
 </div>
