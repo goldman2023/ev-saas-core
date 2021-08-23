7fd9c00fbf27143b880ee64a02eb8778591b9099
wahaha999 <htk052894@outlook.com>
2021-08-23 20:23:30 +0800

    #feature: blade ui kit icons configuration

--- resources/views/livewire/home-search.blade.php
+++ resources/views/livewire/home-search.blade.php
@@ -13,9 +13,7 @@
                 required="required" 
                 class="flex-1 h-10 px-4 m-1 text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none lg:h-12 dark:text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0">
             <button type="submit" class="flex items-center justify-center w-full p-2 m-1 text-white transition-colors duration-200 transform rounded-md lg:w-12 lg:h-12 lg:p-0 bg-indigo-600 hover:bg-indigo-300 focus:outline-none focus:bg-indigo-300">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                </svg>
+                @svg('search', 'w-6 h-6')
             </button>
         </form>
         <!-- End Seach input form -->
