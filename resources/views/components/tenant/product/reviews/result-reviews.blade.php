da9b38a117f89bb9c80fcf30c5f7a9feaea5d567
Eimantas <eim.kasp@gmail.com>
2021-08-23 14:04:28 +0300

    notes after call

--- resources/views/components/tenant/product/reviews/result-reviews.blade.php
+++ resources/views/components/tenant/product/reviews/result-reviews.blade.php
@@ -9,7 +9,7 @@
                     </svg>
                 @endfor
 
-                @for ($i = $average_rating; $i < 5; $i++) 
+                @for ($i = $average_rating; $i < 5; $i++)
                     <svg class="flex-shrink-0 h-5 w-5 text-gray-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                     </svg>
@@ -48,11 +48,13 @@
     </div>
 
     <div class="mt-10">
-        <h3 class="text-lg font-medium text-gray-900">Share your thoughts</h3>
+
+        <h3 class="text-lg font-medium text-gray-900">
+            Share your thoughts</h3>
         <p class="mt-1 text-sm text-gray-600">If you’ve used this product, share your thoughts with other customers</p>
 
         <button @click="open = true" class="mt-6 inline-flex w-full bg-white border border-gray-300 rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-50 sm:w-auto lg:w-full">Write a review</button>
     </div>
-    
+
 </div>
 
--- resources/views/components/tenant/product/reviews/show-reviews.blade.php
+++ resources/views/components/tenant/product/reviews/show-reviews.blade.php
@@ -8,18 +8,28 @@
         <!-- End Review Result Section  -->
 
         <div class="mt-16 lg:mt-0 lg:col-start-6 lg:col-span-7">
-            <h3 class="sr-only">Recent reviews</h3>
+            <h3 class="sr-only">
+                Recent reviews
+            </h3>
             @if(count($product->reviews)>0)
             @foreach($product->reviews as $key => $review)
             <x-tenant.product.reviews.review-card :review="$review "></x-tenant.product.reviews.review-card>
             @endforeach
             @else
-            <p class="mt-5">No reviews</p>
+            <p class="mt-5">
+                {{-- Please use x-label with global parameter --}}
+                <x-label :label="ev_dynamic_translate('No reviews', true)">
+                </x-label>
+
+            </p>
             @endif
         </div>
     </div>
 
     <div x-show="open">
+        {{-- <x-modal> --}}
+            {{-- TODO: Work with @vukasin to figure out correct syntax --}}
+        {{-- </x-modal> --}}
         <x-tenant.product.reviews.add-review-modal :product="$product"></x-tenant.product.reviews.add-review-modal>
     </div>
 </div>
