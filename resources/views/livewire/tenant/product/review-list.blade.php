f4dbd2ead1820e6ed62be6952e611c1995100369
Eimantas <eim.kasp@gmail.com>
2021-08-25 17:31:29 +0300

    refactor label component

--- resources/views/livewire/tenant/product/review-list.blade.php
+++ resources/views/livewire/tenant/product/review-list.blade.php
@@ -8,8 +8,8 @@
         @endforeach
     @else
         <p class="mt-5">
-            <x-label :label="ev_dynamic_translate('No reviews', true)">
-            </x-label>
+            <x-ev.label :label="ev_dynamic_translate('No reviews', true)">
+            </x-ev.label>
         </p>
     @endif
 </div>
