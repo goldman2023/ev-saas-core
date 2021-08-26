47d6dbac7adb41378befbca2f6f32a5869719b09
Eimantas <eim.kasp@gmail.com>
2021-08-26 15:58:28 +0300

    company card updates

--- resources/views/components/company-star-rating.blade.php
+++ resources/views/components/company-star-rating.blade.php
@@ -1,7 +1,7 @@
 @for ($i = 0; $i < $company->user->shop->company_size_calculated(); $i++)
     <span class="company-size-rating">
 
-        <img class="avatar avatar-xss ml-1" src="{{ asset('assets/svg/illustrations/top-vendor.svg') }}" alt="Review rating"
+        <img class="avatar avatar-xss ml-1" src="/assets/svg/illustrations/top-vendor.svg" alt="Review rating"
             data-toggle="tooltip" data-placement="top" title=""
             data-original-title="Company Size {{ $company->user->shop->company_size_calculated() }}/5">
     </span>
