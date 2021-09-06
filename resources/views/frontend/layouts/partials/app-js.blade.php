dfe5e9606f01bd59ea22099bd59a069783a9ab30
Eimantas <eim.kasp@gmail.com>
2021-09-06 17:24:48 +0300

    fix child theme js issue

--- resources/views/frontend/layouts/partials/app-js.blade.php
+++ resources/views/frontend/layouts/partials/app-js.blade.php
@@ -1,3 +1,28 @@
+<script>
+    window.AIZ = window.AIZ || {};
+    window.AIZ.local = {
+        nothing_found: '{{ translate('Nothing found') }}',
+        choose_file: '{{ translate('Choose file') }}',
+        file_selected: '{{ translate('File selected') }}',
+        files_selected: '{{ translate('Files selected') }}',
+        add_more_files: '{{ translate('Add more files') }}',
+        adding_more_files: '{{ translate('Adding more files') }}',
+        drop_files_here_paste_or: '{{ translate('Drop files here, paste or') }}',
+        browse: '{{ translate('Browse') }}',
+        upload_complete: '{{ translate('Upload complete') }}',
+        upload_paused: '{{ translate('Upload paused') }}',
+        resume_upload: '{{ translate('Resume upload') }}',
+        pause_upload: '{{ translate('Pause upload') }}',
+        retry_upload: '{{ translate('Retry upload') }}',
+        cancel_upload: '{{ translate('Cancel upload') }}',
+        uploading: '{{ translate('Uploading') }}',
+        processing: '{{ translate('Processing') }}',
+        complete: '{{ translate('Complete') }}',
+        file: '{{ translate('File') }}',
+        files: '{{ translate('Files') }}',
+    }
+</script>
+
 @if (get_setting('facebook_chat') == 1)
     <script type="text/javascript">
         window.fbAsyncInit = function () {
