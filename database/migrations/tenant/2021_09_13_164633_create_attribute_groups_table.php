fc0a617c12f3e714affe41a9bf9f9a0aeb232d37
wahaha999 <htk052894@outlook.com>
2021-09-13 22:36:22 +0800

    #progress: create attribute group model

---
+++ database/migrations/tenant/2021_09_13_164633_create_attribute_groups_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributeGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_groups', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('name', 255)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_groups');
+    }
+}
