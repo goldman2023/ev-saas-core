7736ffd3e755b31c2cf78e42a652ae60e92c2172
Vukasin Jockovic <jockovicvukasin@gmail.com>
2021-10-29 00:31:24 +0200

    Subcategory cards component full-width inner card part

--- resources/views/components/default/categories/sub-category-cards.blade.php
+++ resources/views/components/default/categories/sub-category-cards.blade.php
@@ -5,8 +5,7 @@
                 <a class="card card-bordered card-hover-shadow h-100"
                    href="{{ route('products.category', $category->slug) }}">
                     <div class="card-body d-flex align-items-center">
-                        <div class="media align-items-center d-block d-sm-flex text-sm-left text-center">
-
+                        <div class="media align-items-center d-block d-sm-flex text-sm-left text-center w-100">
                             {{-- TODO: Show last product image if there is no icon --}}
                             <x-tenant.system.image class="d-inline d-sm-inline avatar avatar-sm avatar-circle mr-sm-3 mb-2 mb-sm-0" :image="$category->icon" fit="cover">
                             </x-tenant.system.image>
