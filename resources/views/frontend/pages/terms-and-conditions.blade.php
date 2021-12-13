dfa2e12ec9850847cf78ea10631ee7ac77e5ac5e
Eimantas <eim.kasp@gmail.com>
2021-12-13 13:54:05 +0200

    Profile picutre update

--- resources/views/frontend/pages/terms-and-conditions.blade.php
+++ resources/views/frontend/pages/terms-and-conditions.blade.php
@@ -1,8 +1,15 @@
 @extends('frontend.layouts.app')
 
 @section('content')
-    <!-- Features Section -->
-    Make this dynamic
-    <!-- End Features Section -->
+<!-- Features Section -->
+<div class="container mt-3">
+    <div class="row">
+        <div class="col-12">
+            <h1> Make this dynamic </h1>
+        </div>
+    </div>
+
+</div>
+<!-- End Features Section -->
 
 @endsection
