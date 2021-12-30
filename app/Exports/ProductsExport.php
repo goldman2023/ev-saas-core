2cdfedcd9fc1191fc0325caf3796a2bcecd0f063
Eimantas <eim.kasp@gmail.com>
2021-12-30 14:28:01 +0200

    Update ProductsExport.php

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -4,9 +4,10 @@ namespace App\Exports;
 
 use App\Models\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
-class ProductsExport implements FromCollection, WithMapping
+class ProductsExport implements FromCollection, WithMapping, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -16,6 +17,22 @@ class ProductsExport implements FromCollection, WithMapping
         return Product::all();
     }
 
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'title',
+            'description',
+            'availability',
+            'condition',
+            'price',
+            'link',
+            'image_link',
+            'brand',
+        ];
+    }
+
     public function map($product): array
     {
         $brand = "inhouse";
@@ -23,11 +40,14 @@ class ProductsExport implements FromCollection, WithMapping
             $brand = $product->brand->name;
         }
         return [
-            $product->name,
-            $product->description,
-            $product->current_stock,
-            $product->condition,
-            $product->price,
+            $product->id,
+            substr($product->name, 0, 149),
+            strip_tags($product->description),
+            /* TODO: Make stock dynamic, how to do in_stock/out_of_stock?  */
+            'in stock',
+            'new',
+            /*  TODO: Make currency dynamic */
+            $product->total_price . " €",
             $product->permalink,
             $product->getThumbnail(),
             $brand
