289503f175600e08f5313edfacec47f57f775401
Eimantas <eim.kasp@gmail.com>
2022-01-10 16:46:06 +0200

    Customer Dashboard WIP

---
+++ resources/views/components/default/dashboard/customer/current-plan.blade.php
@@ -0,0 +1,40 @@
+<div class="card">
+    <!-- Header -->
+    <div class="card-header d-flex justify-content-between align-items-center border-bottom">
+        <h5 class="card-header-title">
+            {{ $title }}
+        </h5>
+
+    </div>
+    <!-- End Header -->
+
+    <!-- Body -->
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md mb-4 mb-md-0">
+                {{ $image }}
+                <div class="mt-3">
+
+
+                    <span class="card-subtitle mb-0">Your plan: </span>
+                    <h5 class="mt-0">{{  translate('Headache Free Website')}}</h5>
+                </div>
+
+
+            </div>
+
+        <hr class='my-3'>
+            <!-- End Col -->
+
+            <div class="col-md-12">
+                <div class="d-grid d-md-flex gap-3">
+                    <a class="btn btn-white btn-sm mr-3" target="_blank" href="https://adeoweb.lt/bolt/login">Manage Website</a>
+                    <a class="btn btn-primary btn-sm btn-transition" target="_blank" href="https://adeoweb.lt/">Visit website</a>
+                </div>
+            </div>
+            <!-- End Col -->
+        </div>
+        <!-- End Row -->
+    </div>
+    <!-- End Body -->
+</div>
---
+++ resources/views/components/default/dashboard/customer/support-agent.blade.php
@@ -0,0 +1,69 @@
+<div class="js-sticky-block card mb-3 mb-lg-5">
+<!-- Header -->
+<div class="card-header">
+ <h5 class="card-header-title">{{ translate('Your Agent') }}</h5>
+</div>
+<!-- End Header -->
+
+<!-- Body -->
+<div class="card-body">
+ <ul class="list-unstyled list-unstyled-py-3 text-dark mb-3">
+
+   <li class="mb-3">
+    <div class="d-flex align-items-center">
+        <a class="d-flex align-items-center mr-2" href="#">
+          <div class="avatar avatar-sm avatar-soft-primary avatar-circle">
+            <span class="avatar-initials">EIM</span>
+            <span class="avatar-status avatar-sm-status avatar-status-success bg-success"></span>
+          </div>
+          <div class="ml-3">
+            <h5 class="text-hover-primary mb-0">Eimantas Kasperiunas</h5>
+            <span class="font-size-sm text-body">2hr response time</span>
+          </div>
+        </a>
+
+      </div>
+   </li>
+
+   <li>
+     <i class="tio-user-outlined nav-icon"></i>
+     Ella Lauda
+   </li>
+   <li>
+     <i class="tio-briefcase-outlined nav-icon"></i>
+     No department
+   </li>
+   <li>
+     <i class="tio-city nav-icon"></i>
+     Htmlstream
+   </li>
+
+   <li class="pt-2 pb-0">
+     <small class="card-subtitle">Contacts</small>
+   </li>
+
+   <li>
+     <i class="tio-online nav-icon"></i>
+     ella@example.com
+   </li>
+   <li>
+     <i class="tio-android-phone-vs nav-icon"></i>
+     +1 (609) 972-22-22
+   </li>
+
+   <li class="pt-2 pb-0">
+     <small class="card-subtitle">Teams</small>
+   </li>
+
+   <li>
+     <i class="tio-group-equal nav-icon"></i>
+     Member of 7 teams
+   </li>
+   <li>
+     <i class="tio-briefcase-outlined nav-icon"></i>
+     Working on 8 projects
+   </li>
+ </ul>
+</div>
+<!-- End Body -->
+</div>
