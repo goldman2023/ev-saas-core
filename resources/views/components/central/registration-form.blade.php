8c7c5cc45e32687bfb9e01ef754ea969c4b89395
Eimantas <eim.kasp@gmail.com>
2022-01-23 20:00:01 +0200

    Reduce necessary fields count

--- resources/views/components/central/registration-form.blade.php
+++ resources/views/components/central/registration-form.blade.php
@@ -38,31 +38,11 @@
         @enderror
     </div>
     <div class="mt-6">
-        <label for="company" class="block text-sm font-medium text-gray-700 leading-5">
-            Company
-        </label>
 
-        <div class="mt-1 rounded-md shadow-sm">
-            <input autocomplete="off" value="{{ old('company', '') }}" name="company" id="company" type="text" required autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('company') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red @enderror" />
-        </div>
-
-        @error('company')
-        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-        @enderror
     </div>
 
     <div class="mt-6">
-        <label for="name" class="block text-sm font-medium text-gray-700 leading-5">
-            Full name
-        </label>
 
-        <div class="mt-1 rounded-md shadow-sm">
-            <input autocomplete="off" value="{{ old('name', '') }}" name="name" id="name" type="text" required autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red @enderror" />
-        </div>
-
-        @error('name')
-        <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-        @enderror
     </div>
 
 
@@ -96,8 +76,12 @@
     <div class="mt-6">
                     <span class="block w-full rounded-md shadow-sm">
                         <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                            Register
+                            {{ translate('Get Started') }}
                         </button>
                     </span>
+
+                    <p class="mt-2 text-xs text-gray-600">
+                        {{ translate("Initial setup takes less than 5 minutes!") }}
+                    </p>
     </div>
 </form>
