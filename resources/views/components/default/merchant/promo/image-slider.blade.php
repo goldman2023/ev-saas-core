db7190cf03444bfac34cb2dd1e3387a7f2cc105b
Eimantas <eim.kasp@gmail.com>
2022-01-26 05:46:29 +0200

    Update image-slider.blade.php

--- resources/views/components/default/merchant/promo/image-slider.blade.php
+++ resources/views/components/default/merchant/promo/image-slider.blade.php
@@ -1,9 +1,9 @@
 {{-- TODO: make this dynamic by single vendor --}}
 <div class="row">
-    <div class="col-12">
+    <div class="col-12 mb-3">
         <div class="js-slide bg-img-hero-center"
             style="background-image: url(https://www.dantesports.com/pub/media/responsivebannerslider/s/m/smith_wesson_m_p12_pre-order_english.jpg); background-size: cover;">
-            <div class="space-2 space-lg-5">
+            <div class="space-2 space-lg-4">
             </div>
         </div>
     </div>
@@ -11,7 +11,7 @@
     <div class="col-6">
         <div class="js-slide bg-img-hero-center"
             style="background-image: url(https://www.dantesports.com/pub/media/responsivebannerslider/b/e/benelli-super-nova-tactical-combo-english.jpg);">
-            <div class="space-2 space-lg-5">
+            <div class="space-2 space-lg-3">
             </div>
         </div>
     </div>
@@ -19,7 +19,7 @@
     <div class="col-6">
         <div class="js-slide bg-img-hero-center"
         style="background-image: url(https://www.dantesports.com/pub/media/responsivebannerslider/f/a/falco-arms-banner-english-final.jpg);">
-        <div class="space-2 space-lg-5">
+        <div class="space-2 space-lg-3">
         </div>
     </div>
     </div>
