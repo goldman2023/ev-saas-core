6daddde2fedbdd99b39d415a04eb44f137e067d3
vukasinjockovic <jockovicvukasin@gmail.com>
2022-02-01 17:26:59 +0100

    [Breadcrumbs] Standardization and logic

---
+++ routes/breadcrumbs/tenant.php
@@ -0,0 +1,30 @@
+<?php
+
+// Note: Laravel will automatically resolve `Breadcrumbs::` without
+// this import. This is nice for IDE syntax and refactoring.
+use App\Models\Category;
+use Diglactic\Breadcrumbs\Breadcrumbs;
+
+// This import is also not required, and you could replace `BreadcrumbTrail $trail`
+//  with `$trail`. This is nice for IDE type checking and completion.
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+
+// Home
+Breadcrumbs::for('home', function (BreadcrumbTrail $trail) {
+    $trail->push('Home', route('home'));
+});
+
+// Category
+Breadcrumbs::for('category', function (BreadcrumbTrail $trail, Category $category, $content_type = null) {
+    $trail->parent('home');
+
+    $ancestors = $category->ancestors;
+
+    if(!empty($ancestors)) {
+        foreach ($ancestors as $ancestor) {
+            $trail->push($ancestor->name, $ancestor->getPermalink($content_type));
+        }
+    }
+
+    $trail->push($category->name, $category->getPermalink($content_type));
+});
