9a335bc00b9146a7fbf9e4542611a2308ceb48d9
vukasinjockovic <jockovicvukasin@gmail.com>
2022-02-02 03:11:29 +0100

    [Migrations and Models] Blog posts, relationships and translations

---
+++ database/migrations/tenant/2022_02_02_031615_create_blog_posts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('shop_id')->nullable();
+            $table->string('title', 200);
+            $table->string('slug', 200)->unique();
+            $table->string('excerpt', 300)->nullable();
+            $table->longText('content')->nullable();
+            $table->string('status', 50)->default('draft')->index(); // can be: published, draft, private, pending
+            $table->tinyInteger('subscription_only')->default(0)->index();
+            $table->string('meta_title', 255)->nullable();
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('created_at');
+            $table->index('updated_at');
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
