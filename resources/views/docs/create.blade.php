6a36539aa34486325405f12b79ce3e533a18c1aa
Eimantas <eim.kasp@gmail.com>
2022-02-09 21:42:47 +0200

    remove unused components

--- resources/views/docs/create.blade.php
+++ resources/views/docs/create.blade.php
@@ -9,8 +9,7 @@
                 <div class="col-12">
                     <h3> {{ translate('Create New Component') }}</h3>
 
-                    <x-default.forms.product-form>
-                    </x-default.forms.product-form>
+
 
                     <div id="app">
                         {!! $form->render() !!}
