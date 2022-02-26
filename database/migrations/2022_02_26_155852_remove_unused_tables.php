2b4e3bb8f747296942480e5d07d23aef2339752b
Eimantas <eim.kasp@gmail.com>
2022-02-26 16:23:58 +0200

    Remove Unused Migrations

--- database/migrations/2022_02_26_155852_remove_unused_tables.php
+++ database/migrations/2022_02_26_155852_remove_unused_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeEventsTable extends Migration
+class RemoveUnusedTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,7 @@ class ChangeEventsTable extends Migration
      */
     public function up()
     {
-        Schema::table('events', function (Blueprint $table) {
-            $table->string('slug', 255)->after('id')->unique();
-        });
+        //
     }
 
     /**
@@ -25,8 +23,6 @@ class ChangeEventsTable extends Migration
      */
     public function down()
     {
-        Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn('slug');
-        });
+        //
     }
 }
