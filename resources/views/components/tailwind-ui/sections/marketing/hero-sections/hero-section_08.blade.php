67f31c46ec09c0898f3d5627aa8029d1ad171a0e
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-03-17 10:17:20 +0000

    [WeEdit] Image custom field and fe component

--- resources/views/components/tailwind-ui/sections/marketing/hero-sections/hero-section_08.blade.php
+++ resources/views/components/tailwind-ui/sections/marketing/hero-sections/hero-section_08.blade.php
@@ -60,17 +60,19 @@
             <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
             <rect x="118" width="404" height="784" fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)" />
           </svg>
-          <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
-            <button type="button" class="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <span class="sr-only">Watch our video to learn more</span>
-              <img class="w-full" src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="">
-              <div class="absolute inset-0 w-full h-full flex items-center justify-center" aria-hidden="true">
-                <svg class="h-20 w-20 text-indigo-500" fill="currentColor" viewBox="0 0 84 84">
-                  <circle opacity="0.9" cx="42" cy="42" r="42" fill="white" />
-                  <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
-                </svg>
-              </div>
-            </button>
+          
+          {{-- Hero Image--}}
+          <div we-slot name="image_slot" we-title="Hero Image" class="w-full mt-4">
+            <x-ev.image
+                we-name="hero_image"
+                we-title="Image"
+                class="w-full lg:max-w-[540px] {{ $weData['image_slot']['components']['hero_image']['data']['class'] ?? '' }}"
+                :href="$weData['image_slot']['components']['hero_image']['data']['href'] ?? null"
+                :target="$weData['image_slot']['components']['hero_image']['data']['target'] ?? null"
+                :options="['w' => 600]"
+                :alt-text="$weData['image_slot']['components']['hero_image']['data']['alt_text'] ?? ''"
+                :src="$weData['image_slot']['components']['hero_image']['data']['src'] ?? ''">
+            </x-ev.image>
           </div>
         </div>
 
