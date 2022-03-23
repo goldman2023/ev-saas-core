301cb58bb9b5be91f9c315d2a69d6bab510c1ece
Eimantas <eim.kasp@gmail.com>
2022-03-23 17:34:57 +0200

    Update feed-sidebar.blade.php

--- resources/views/components/feed/elements/feed-sidebar.blade.php
+++ resources/views/components/feed/elements/feed-sidebar.blade.php
@@ -25,7 +25,17 @@
             </svg>
             <span class="truncate"> {{ translate('Shops') }} </span>
         </a>
-
+        <a href="#"
+            class="text-gray-600 hover:bg-gray-50 group flex items-center px-3 py-2 text-sm font-medium rounded-md">
+            <!-- Heroicon name: outline/user-group -->
+            <svg class="text-gray-400 group-hover:text-gray-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+            <span class="truncate"> {{ translate('Products') }} </span>
+        </a>
         <a href="#"
             class="text-gray-600 hover:bg-gray-50 group flex items-center px-3 py-2 text-sm font-medium rounded-md">
             <!-- Heroicon name: outline/user-group -->
