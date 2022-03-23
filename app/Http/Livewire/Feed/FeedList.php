3350d859a846fdeb5a44a9d1ba810365846dcbe4
Eimantas <eim.kasp@gmail.com>
2022-03-23 17:32:21 +0200

    Add pagination

--- app/Http/Livewire/Feed/FeedList.php
+++ app/Http/Livewire/Feed/FeedList.php
@@ -3,22 +3,34 @@
 namespace App\Http\Livewire\Feed;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use Spatie\Activitylog\Models\Activity;
 
 class FeedList extends Component
 {
-    public $activities;
+    use WithPagination;
+
+    protected $activitiesObjects;
     public $perPage = 10;
+    public $links;
 
     public function mount()
     {
         $this->perPage = 10;
-        $this->activities = Activity::take(50)->whereNotIn('description', ['viewed', 'deleted'])->orderBy('created_at', 'desc')->get();
+        $this->activitiesObjects = Activity::whereNotIn('description', ['viewed', 'deleted'])->orderBy('created_at', 'desc');
+
     }
+
     public function render()
     {
+
         return view('livewire.feed.feed-list', [
-            'activities' => $this->activities,
+            'activities' => $this->activitiesObjects->paginate($this->perPage),
         ]);
+    }
+
+    public function loadMore()
+    {
+        $this->perPage += 10;
     }
 }
--- resources/views/livewire/feed/feed-list.blade.php
+++ resources/views/livewire/feed/feed-list.blade.php
@@ -1,3 +1,5 @@
-@foreach($activities as $key => $item)
+@foreach($activities as $item)
 <x-feed.elements.feed-card :item="$item"></x-feed.elements.feed-card>
 @endforeach
+
+{{ $activities->links() }}
