ecdcd2aff44141f1017fce6401f002eb16fb3c97
Eimantas <eim.kasp@gmail.com>
2022-03-28 23:46:36 +0300

    Category suggestion list update

---
+++ app/Http/Livewire/Onboarding/Elements/CategorySuggestionList.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire\Onboarding\Elements;
+
+use App\Models\Category;
+use Livewire\Component;
+
+class CategorySuggestionList extends Component
+{
+    public $categories;
+    public function mount() {
+        $this->categories = Category::whereHas('products')->get();
+    }
+    public function render()
+    {
+        return view('livewire.onboarding.elements.category-suggestion-list');
+    }
+}
