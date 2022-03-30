e0174dd81a00eefb81e268a5acd747f9ea8a25c0
Eimantas <eim.kasp@gmail.com>
2022-03-30 19:16:22 +0300

    File upload issue test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
 
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
 
         $this->registerCustomValidaionRules();
     }
