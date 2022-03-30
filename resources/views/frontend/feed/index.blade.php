860ea65079759a5e738138dedf8d4517a0a76c71
Eimantas <eim.kasp@gmail.com>
2022-03-31 02:11:10 +0300

    tailwind config update

--- resources/views/frontend/feed/index.blade.php
+++ resources/views/frontend/feed/index.blade.php
@@ -85,7 +85,7 @@
                                                 </span>
                                             </a>
                                             <button type="submit"
-                                                class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                                class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                                                 {{ translate('Add a post') }}
                                             </button>
                                         </div>
--- resources/views/livewire/feed/feed-list.blade.php
+++ resources/views/livewire/feed/feed-list.blade.php
@@ -18,7 +18,7 @@
                     class="@if($type == 'recent') text-gray-900 @else text-gray-500 hover:text-gray-700 @endif  rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
                     <span>Recent</span>
                     @if($type == 'recent')
-                        <span aria-hidden="true" class="bg-rose-500 absolute inset-x-0 bottom-0 h-0.5"></span>
+                        <span aria-hidden="true" class="bg-indigo absolute inset-x-0 bottom-0 h-0.5"></span>
                     @else
                         <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
                     @endif
@@ -28,7 +28,7 @@
                     class="@if($type == 'trending') text-gray-900 @else text-gray-500 hover:text-gray-70 @endif group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-6 text-sm font-medium text-center hover:bg-gray-50 focus:z-10">
                     <span>{{ translate('Trending') }}</span>
                     @if($type == 'trending')
-                        <span aria-hidden="true" class="bg-rose-500 absolute inset-x-0 bottom-0 h-0.5"></span>
+                        <span aria-hidden="true" class="bg-indigo absolute inset-x-0 bottom-0 h-0.5"></span>
                     @else
                         <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
                     @endif
