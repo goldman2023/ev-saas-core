f5563475b1db0cad6e957b44419fe30a3e127fe1
Eimantas <eim.kasp@gmail.com>
2022-03-31 14:03:41 +0300

    Update step4.blade.php

--- resources/views/frontend/onboarding/step4.blade.php
+++ resources/views/frontend/onboarding/step4.blade.php
@@ -17,8 +17,8 @@
             <div class="bg-white">
                 <div class="max-w-3xl mx-auto">
                     <div class="max-w-xl">
-                        <h1 class="text-sm font-semibold uppercase tracking-wide text-indigo-600">{{ translate('Thank
-                            you!') }}</h1>
+                        <h1 class="text-sm font-semibold uppercase tracking-wide text-indigo-600">
+                            {{ translate('Thank you!') }}</h1>
                         <p class="mt-2 text-4xl font-extrabold tracking-tight sm:text-5xl">{{ translate('You have
                             succesfully joined') }} {{ get_site_name() }}
                             <span class="emoji ml-2">🎉</span>
@@ -30,15 +30,155 @@
                     <div class="grid grid-cols-2 gap-10">
                         <div class="">
                             <dl class="mt-12 mb-6 text-sm font-medium  ">
-                                <dt class="text-gray-900">{{ translate('Personal Profile Preview') }}</dt>
+                                <h2 class="text-lg font-medium text-gray-900">
+                                    {{ translate('Personal Profile Preview') }}
+                                </h2>
+                                <p class="x">
+                                    {{ translate('This is how your profile will be shown') }}
+
+                                </p>
                             </dl>
                             <div class="border-t border-gray-200">
                                 <x-default.elements.user-card :user="auth()->user()"></x-default.elements.user-card>
                             </div>
+                            <div class='mt-3 text-sm text-gray-500'>
+                                <a href="{{ auth()->user()->getPermalink() }}">
+                                  <span class="emoji mr-1">🌎</span>  {{ translate('View your public profile page') }}
+                                </a>
+                            </div>
+
 
                         </div>
 
                         <div class="">
+                            <!-- This example requires Tailwind CSS v2.0+ -->
+                            <div class="max-w-lg mx-auto mt-12">
+                                <h2 class="text-lg font-medium text-gray-900">
+                                    {{ translate('Get started on ') }} {{ get_site_name() }}
+                                </h2>
+                                <p class="mt-0 text-sm text-gray-500">
+                                    {{ translate('Explore our features and social feed') }}
+                                </p>
+                                <ul role="list" class="mt-6 border-t border-b border-gray-200 divide-y divide-gray-200">
+                                    <li>
+                                        <div class="relative group py-4 flex items-start space-x-3">
+                                            <div class="flex-shrink-0">
+                                                <span
+                                                    class="inline-flex items-center justify-center h-10 w-10 rounded-lg bg-pink-500">
+                                                    <!-- Heroicon name: outline/speakerphone -->
+                                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                                        aria-hidden="true">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+                                                    </svg>
+                                                </span>
+                                            </div>
+                                            <div class="min-w-0 flex-1">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    <a href="{{ route('feed.index') }}">
+                                                        <span class="absolute inset-0" aria-hidden="true"></span>
+                                                        {{ translate('Explore Feed') }}
+                                                    </a>
+                                                </div>
+                                                <p class="text-sm text-gray-500">
+                                                    {{ translate('Explore our social knowledge feed') }}
+                                                </p>
+                                            </div>
+                                            <div class="flex-shrink-0 self-center">
+                                                <!-- Heroicon name: solid/chevron-right -->
+                                                <svg class="h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                    fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd"
+                                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <div class="relative group py-4 flex items-start space-x-3">
+                                            <div class="flex-shrink-0">
+                                                <span
+                                                    class="inline-flex items-center justify-center h-10 w-10 rounded-lg bg-purple-500">
+                                                    <!-- Heroicon name: outline/terminal -->
+                                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                                        aria-hidden="true">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                                    </svg>
+                                                </span>
+                                            </div>
+                                            <div class="min-w-0 flex-1">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    <a href="{{ route('product.create') }}">
+                                                        <span class="absolute inset-0" aria-hidden="true"></span>
+                                                        {{ translate('Add a product') }}
+                                                    </a>
+                                                </div>
+                                                <p class="text-sm text-gray-500">
+                                                    {{ translate('Add services and products so you can share your
+                                                    knowledge and earn') }}
+                                                </p>
+                                            </div>
+                                            <div class="flex-shrink-0 self-center">
+                                                <!-- Heroicon name: solid/chevron-right -->
+                                                <svg class="h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                    fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd"
+                                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                    <li>
+                                        <div class="relative group py-4 flex items-start space-x-3">
+                                            <div class="flex-shrink-0">
+                                                <span
+                                                    class="inline-flex items-center justify-center h-10 w-10 rounded-lg bg-yellow-500">
+                                                    <!-- Heroicon name: outline/calendar -->
+                                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                                        aria-hidden="true">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            stroke-width="2"
+                                                            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                                    </svg>
+                                                </span>
+                                            </div>
+                                            <div class="min-w-0 flex-1">
+                                                <div class="text-sm font-medium text-gray-900">
+                                                    <a href="{{ route('feed.products') }}">
+                                                        <span class="absolute inset-0" aria-hidden="true"></span>
+                                                        {{ translate('Events and Courses') }}
+                                                    </a>
+                                                </div>
+                                                <p class="text-sm text-gray-500">
+                                                    {{ translate('Explore events and courses from FoxAsk community') }}
+                                                </p>
+                                            </div>
+                                            <div class="flex-shrink-0 self-center">
+                                                <!-- Heroicon name: solid/chevron-right -->
+                                                <svg class="h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                                    fill="currentColor" aria-hidden="true">
+                                                    <path fill-rule="evenodd"
+                                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
 
 
                         </div>
