887e0c40431521418b1453378548144ba52dae49
Eimantas <eim.kasp@gmail.com>
2022-03-31 13:28:06 +0300

    Update product-card.blade.php

--- resources/views/livewire/feed/elements/product-card.blade.php
+++ resources/views/livewire/feed/elements/product-card.blade.php
@@ -23,10 +23,8 @@
           href="{{  $product->getPermalink() }}"
           class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
             <!-- Heroicon name: solid/mail -->
-            <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-              </svg>
+            {{ svg('heroicon-s-eye', ['class'=> 'max-w-[20px] text-gray-900']) }}
+
             <span class="ml-3">
                 {{ translate('View') }}
             </span>
