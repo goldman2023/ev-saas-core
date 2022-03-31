c919f9850f9699872cb480fc7dc5ae7f54d47564
Eimantas <eim.kasp@gmail.com>
2022-03-31 13:26:51 +0300

    Update shop card

--- resources/views/livewire/actions/wishlist-button.blade.php
+++ resources/views/livewire/actions/wishlist-button.blade.php
@@ -1,9 +1,9 @@
-<div>
+<div class="relative w-0 flex-1 inline-flex items-center justify-center text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
 
     <div wire:click="addToWishlist()">
 
         <button
-            class="btn btn-secondary @if($added) btn-danger @endif align-items-center d-flex justify-content-center align-items-center">
+            class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
             @if($added)
             {{ svg($iconActive, ['class'=> 'max-w-[16px] text-gray-900 mr-2']) }}
             {{ $available_actions[$action] }}
