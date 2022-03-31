8b8d0a670e93dc2c861dd79c6694b14329c8975a
Eimantas <eim.kasp@gmail.com>
2022-03-31 10:17:13 +0300

    Login Fix

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Spatie\Activitylog\Models\Activity;
@@ -15,11 +16,13 @@ class FeedController extends Controller
 
     public function shops() {
         $shops = Shop::all();
-        return view('frontend.feed.shops', compact('shops'));
+        $users = Shop::orderBy('created_at', 'desc')->all();
+
+        return view('frontend.feed.shops', compact(['shops', 'users']));
     }
 
     public function products() {
-        $shops = Shop::orderBy('created_at', 'desc')->all();
-        return view('frontend.feed.products');
+        $products = Product::orderBy('created_at', 'desc')->all();
+        return view('frontend.feed.products', compact('products'));
     }
 }
