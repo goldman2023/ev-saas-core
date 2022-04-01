960f11b6eb16e0640031a7208dc7632787aac75c
Eimantas <eim.kasp@gmail.com>
2022-04-01 12:51:10 +0300

    Update logo.blade.php

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -2,5 +2,5 @@
 <x-tenant.system.image alt="{{ get_site_name() }} logo"
 width="{{ $width ?? '126' }}" height="{{ $height ?? '24' }}"
 class="h-8 w-auto sm:h-10 fill-current"
-    :image="get_tenant_setting('header_logo')">
+    :image="get_site_logo()">
 </x-tenant.system.image>
