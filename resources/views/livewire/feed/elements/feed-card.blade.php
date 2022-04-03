ce3fdf35973b514ab72562c9f512519b2b101463
Eimantas <eim.kasp@gmail.com>
2022-04-03 17:52:27 +0300

    Feed Card refactoring

--- resources/views/livewire/feed/elements/feed-card.blade.php
+++ resources/views/livewire/feed/elements/feed-card.blade.php
@@ -4,104 +4,9 @@
         <article class='mb-3 bg-white px-4 py-6 shadow sm:p-6 sm:rounded-lg'  x-on:click="isModalOpen = true">
             {{-- x-intersect:visible="$wire.track_impression({{ $item->id }})" --}}
             <div>
-                <div class="flex space-x-3">
-                    <div class="flex-shrink-0">
-                        <div class="inline-block relative">
-                            {{-- TODO: Implement quick view for person and for product click --}}
-                            <x-tenant.system.image alt="{{ get_site_name() }} logo"
-                                class="h-12 w-12 rounded-full bg-white ring-2 ring-indigo-600" fit="contain" :image="$item->causer->getAvatar()">
-                            </x-tenant.system.image>
-                            <span
-                                class="absolute bottom-0 right-0 block h-4 w-4 rounded-full ring-2 ring-white bg-green-400"></span>
-                        </div>
-
-                    </div>
-                    <div class="min-w-0 flex-1">
-                        <p class="text-sm font-medium text-gray-900">
-                            @isset( $item->causer->name)
-                            <a href="{{ $item->causer->shop }}" class="hover:underline">
-                                {{ $item->causer->name }}
-                            </a>
-                            @endisset
-                        </p>
-                        <p class="text-sm text-gray-500">
-                            <a href="#" class="hover:underline">
-                                <time datetime="2020-12-09T11:43:00">
-                                    {{ $item->created_at->diffForHumans() }}
-                                </time>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="flex-shrink-0 self-center flex">
-                        <div class="relative inline-block text-left">
-                            <div>
-                                <button type="button"
-                                    class="-m-2 p-2 rounded-full flex items-center text-gray-400 hover:text-gray-600"
-                                    id="options-menu-0-button" aria-expanded="false" aria-haspopup="true">
-                                    <span class="sr-only">Open options</span>
-                                    <!-- Heroicon name: solid/dots-vertical -->
-                                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                        fill="currentColor" aria-hidden="true">
-                                        <path
-                                            d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
-                                    </svg>
-                                </button>
-                            </div>
-
-                            <!--
-                  Dropdown menu, show/hide based on menu state.
-
-                  Entering: "transition ease-out duration-100"
-                    From: "transform opacity-0 scale-95"
-                    To: "transform opacity-100 scale-100"
-                  Leaving: "transition ease-in duration-75"
-                    From: "transform opacity-100 scale-100"
-                    To: "transform opacity-0 scale-95"
-                -->
-                            <div x-data="{ isOpen: false }" x-cloak
-                                class="hidden origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                role="menu" aria-orientation="vertical" aria-labelledby="options-menu-0-button"
-                                tabindex="-1">
-                                <div class="py-1" role="none">
-                                    <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                    <a href="#" class="text-gray-700 flex px-4 py-2 text-sm" role="menuitem"
-                                        tabindex="-1" id="options-menu-0-item-0">
-                                        <!-- Heroicon name: solid/star -->
-                                        <svg class="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path
-                                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                                        </svg>
-                                        <span>Add to favorites</span>
-                                    </a>
-                                    <a href="#" class="text-gray-700 flex px-4 py-2 text-sm" role="menuitem"
-                                        tabindex="-1" id="options-menu-0-item-1">
-                                        <!-- Heroicon name: solid/code -->
-                                        <svg class="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd"
-                                                d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                        <span>Embed</span>
-                                    </a>
-                                    <a href="#" class="text-gray-700 flex px-4 py-2 text-sm" role="menuitem"
-                                        tabindex="-1" id="options-menu-0-item-2">
-                                        <!-- Heroicon name: solid/flag -->
-                                        <svg class="mr-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                            <path fill-rule="evenodd"
-                                                d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                        <span>Report content</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <h2 id="question-title-81614" class="mt-4 text-base font-medium text-gray-900">
+                <x-feed.elements.card-header-user-info :item="$item">
+                </x-feed.elements.card-header-user-info>
+                <h2  class="mt-4 text-base font-medium text-gray-900">
 
                     @if($item->subject_type == 'App\Models\Wishlist')
 
@@ -246,7 +151,6 @@
             </div>
         </article>
     </div>
-    <button>Open Modal</button>
     <livewire:feed.elements.quick-views.main :item="$item">
     </livewire:feed.elements.quick-views.main>
 
