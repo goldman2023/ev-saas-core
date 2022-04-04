d8cc3c32808ae44c6d4d0fadb02e44415847cfef
Eimantas <eim.kasp@gmail.com>
2022-04-05 02:29:38 +0300

    Image error updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -177,7 +177,7 @@ class HomeController extends Controller
             /* Check if feed is disabled for this tenant */
 
             /* Example usage of TenantSetting for feature detection */
-            if(get_tenant_setting('feed_enabled', false)) {
+            if(get_tenant_setting('feed_enabled', true)) {
                 if(auth()->user()) {
                     return redirect()->route('feed.index');
                 } else {
