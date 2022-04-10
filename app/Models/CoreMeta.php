b108cdcc40617f7318e3181816fafee1c992594d
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-04-10 20:42:40 +0000

    CoreMeta Product Types and other data BE

--- app/Models/CoreMeta.php
+++ app/Models/CoreMeta.php
@@ -16,4 +16,38 @@ class CoreMeta extends Model
     public function subject() {
         return $this->morphTo("subject");
     }
+
+    public static function metaDataTypes() {
+        return [
+            'date_type' => 'string',
+            'start_date' => 'date',
+            'end_date' => 'date',
+            'location_type' => 'string',
+            'location_address' => 'string',
+            'location_address_coordinates' => 'array',
+            'location_link' => 'string',
+            'unlockables' => 'array',
+        ];
+    }
+
+    public static function getMeta($core_meta) {
+        $core_meta = $core_meta->keyBy('key')->toArray();
+        $data_types = self::metaDataTypes();
+
+        castValuesForGet($core_meta, $data_types);
+        
+        $missing = array_diff_key($data_types, $core_meta);
+        $missing_clone = $missing;
+        
+        if(!empty($missing)) {
+            foreach($missing as $key => $type) {
+                $missing[$key] = [
+                    'value' => null
+                ];
+            }
+            castValuesForGet($missing, $missing_clone);
+        }
+
+        return array_merge($core_meta, $missing);
+    }
 }
