51d0a9ac2e4d6c8cc3da9af78b8948fdf77b6cb3
Eimantas <eim.kasp@gmail.com>
2022-04-11 14:05:25 +0300

    Update user-welcome.blade.php

--- resources/views/components/dashboard/widgets/user-welcome.blade.php
+++ resources/views/components/dashboard/widgets/user-welcome.blade.php
@@ -90,7 +90,7 @@
                     <span class="emoji mr-2">💻 </span> {{ translate("Manage Shop") }}
                 </a>
                 @else
-                <a type="button" href="{{ route('onboarding.step-3') }}"
+                <a type="button" href="{{ route('onboarding.step3') }}"
                     class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
                     <span class="emoji mr-2">📦 </span> {{ translate("Setup your store") }}
                 </a>
