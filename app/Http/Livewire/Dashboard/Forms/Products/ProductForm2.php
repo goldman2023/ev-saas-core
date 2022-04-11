2eb0130b928818f095f71929abaf7768c6141c55
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-04-11 08:39:44 +0000

    Added exclude_unless to required event core meta fields (ProductForm)

--- app/Http/Livewire/Dashboard/Forms/Products/ProductForm2.php
+++ app/Http/Livewire/Dashboard/Forms/Products/ProductForm2.php
@@ -120,10 +120,10 @@ class ProductForm2 extends Component
             ],
             'meta' => [
                 // TODO: Add proper conditional validation!
-                'core_meta.date_type.value' => [Rule::in(['range', 'specific'])], // range, specific
-                'core_meta.start_date.value' => 'required_if:product.type,event|date',
+                'core_meta.date_type.value' => ['exclude_unless:product.type,event', Rule::in(['range', 'specific'])], // range, specific
+                'core_meta.start_date.value' => ['exclude_unless:product.type,event', 'required_if:product.type,event', 'date'], //'required_if:product.type,event|date',
                 'core_meta.end_date.value' => 'nullable|date',
-                'core_meta.location_type.value' => [Rule::in(['remote', 'offline'])], // remote, location
+                'core_meta.location_type.value' => ['exclude_unless:product.type,event', Rule::in(['remote', 'offline'])], // remote, location
                 'core_meta.location_address.value' => 'nullable',
                 'core_meta.location_address_coordinates.value' => 'nullable',
                 'core_meta.location_link.value' => 'nullable',
