47256fc28236dbb4f1694793c45661cd80eb724a
Eimantas <eim.kasp@gmail.com>
2022-04-19 22:38:38 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Central\CentralController;
+use App\Http\Controllers\Central\RegisterTenantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/refresh-csrf', function() {
