27f76aaba36f0eed586bbeb670fb3b6cfe3ee4a8
Eimantas <eim.kasp@gmail.com>
2022-04-19 19:55:41 +0300

    Update Parent Domain for Tenant Creation

--- config/tenancy.php
+++ config/tenancy.php
@@ -9,7 +9,8 @@ $central_domains = [
     //'ev-saas.com',
     //'ev-saas.test',
     //'localhost',
-    /* Production */];
+    /* Production */
+];
 
 $central_domains[] = 'we-saas.com';
 $central_domains[] = 'app.we-saas.com';
@@ -29,6 +30,8 @@ return [
      */
 
     'central_domains' => $central_domains,
+
+    'parent_domain' =>  env('PARENT_DOMAIN', 'ev-saas.com'),
 
 
 
