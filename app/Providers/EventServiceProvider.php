b7e30628db439afddcd651e1ecf69e0cb485c438
Shift <shift@laravelshift.com>
2022-04-20 11:10:41 +0000

    Shift core files

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -71,4 +71,15 @@ class EventServiceProvider extends ServiceProvider
         // Register All Observers
         $this->registerObservers();
     }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     *
+     * @return bool
+     */
+    public function shouldDiscoverEvents()
+    {
+        return false;
+    }
 }
+
