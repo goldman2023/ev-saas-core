04665e7e33af88a4dfdc8d27d21701a4aaa145f9
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-05-04 14:42:18 +0000

    Quick view (Product) thumbnail and layout fix

--- resources/views/components/dashboard/widgets/user-welcome.blade.php
+++ resources/views/components/dashboard/widgets/user-welcome.blade.php
@@ -97,7 +97,7 @@
                 <a
                 href="https://buy.stripe.com/eVacMRd2x7UB3jW288"
                 target="_blank"
-                type="button" href=" {{ route('custom-pages.show_custom_page', ['pricing']) }}"
+                type="button" href=" {{ route('my.plans.management') }}"
                     class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
                     {{ translate('Join') }} {{ get_site_name() }} {{ translate('PRO') }}
                 </a>
--- resources/views/livewire/feed/elements/quick-views/main.blade.php
+++ resources/views/livewire/feed/elements/quick-views/main.blade.php
@@ -12,7 +12,7 @@
         <div
             class="flex text-base text-left transform transition w-full md:inline-block md:max-w-2xl md:px-4 md:my-8 md:align-middle lg:max-w-4xl">
             <div
-                class="w-full relative flex items-center bg-white px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                class="w-full relative flex items-center bg-white rounded-lg px-4 pt-14 pb-8 overflow-hidden shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
                 <button x-on:click="isModalOpen = false" type="button"
                     class="absolute top-4 right-4 text-gray-400 hover:text-gray-500 sm:top-8 sm:right-6 md:top-6 md:right-6 lg:top-8 lg:right-8">
                     <span class="sr-only">Close</span>
@@ -30,14 +30,12 @@
 
                     <div class="sm:col-span-4 lg:col-span-5">
                         <div class="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden">
-                            {{-- <x-galleries.main-gallery class="object-center object-cover" template="product-gallery"
-                                :model="$item->subject" class="">
+                            <x-galleries.main-gallery template="product-gallery" :model="$item->subject" img-class="object-center object-cover" :show-gallery="false">
                             </x-galleries.main-gallery>
-                            --}}
                         </div>
                     </div>
                     @endif
-                    <div class="sm:col-span-8 lg:col-span-7">
+                    <div class="sm:col-span-8 lg:col-span-7 h-full flex flex-col">
                         <h2 class="text-2xl font-extrabold text-gray-900 sm:pr-12">
                             {{ $item->subject->name }}
                         </h2>
@@ -50,7 +48,7 @@
                                     <x-feed.elements.card-header-user-info :item="$item">
                                     </x-feed.elements.card-header-user-info>
                                 </div>
-                                <div>
+                                <div class="flex items-center justify-end">
                                     @livewire('actions.wishlist-button', [
                                     'object' => $item->causer,
                                     'action' => 'Follow'
@@ -58,42 +56,43 @@
                                 </div>
                             </div>
 
-
-
-                            <div class="mt-6">
-                                <h4 class="sr-only">Description</h4>
-
-                                <p class="text-sm text-gray-700">
-                                    {!! $item->subject->content !!}
-                                </p>
-                            </div>
+                            @if(!empty($item->subject->excerpt))
+                                <div class="mt-6">
+                                    <p class="text-sm text-gray-700">
+                                        {{ $item->subject->excerpt }}
+                                    </p>
+                                </div>
+                            @endif
                         </section>
 
-                        <section aria-labelledby="options-heading" class="mt-6">
+                        <section aria-labelledby="options-heading" class="mt-6 mb-4">
                             {{-- <h3 id="options-heading" class="sr-only">Product options</h3> --}}
 
-                            <div class="mt-6">
-
+                            <div class="w-full">
 
                                 <livewire:actions.wishlist-button
                                     class="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
                                     wire:key="post_{{ $item->id }}" action="Like" :object="$item->subject">
                                 </livewire:actions.wishlist-button>
 
+
                             </div>
-                            @if($item->subject->getPermalink())
-                            <p class="text-center sm:static sm:mt-6 mb-3">
+                            
+                        </section>
+
+                        @if($item->subject->getPermalink())
+                            <div class="w-full text-center sm:static mb-3 mt-auto">
                                 <a href="{{ $item->subject->getPermalink() }}"
                                     class="font-medium text-indigo-600 hover:text-indigo-500">
                                     {{ translate('View full details') }}
                                 </a>
-                            </p>
-                            @endif
-                        </section>
-                        <div class="we-social-comments">
+                            </div>
+                        @endif
+                        
+                        {{-- <div class="we-social-comments">
                             <livewire:actions.social-comments :item="$item">
                             </livewire:actions.social-comments>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
