58affe0929b2d678c5d978010a103ec14eb987d0
Eimantas <eim.kasp@gmail.com>
2022-05-12 23:05:25 +0300

    Refactor guest states

--- resources/views/livewire/feed/feed-list.blade.php
+++ resources/views/livewire/feed/feed-list.blade.php
@@ -4,7 +4,7 @@
             <label for="question-tabs" class="sr-only">Select a tab</label>
             <select id="question-tabs"
                 class="block w-full rounded-md border-gray-300 text-base font-medium text-gray-900 shadow-sm focus:border-rose-500 focus:ring-rose-500">
-                
+
                 <option selected>{{ translate('Recent') }}</option>
                 <option>{{ translate('Trending') }}</option>
 
@@ -15,9 +15,9 @@
         {{-- <div class="hidden sm:block">
             <nav class="relative grid grid-cols-12 gap-3 mb-3">
                 <button wire:click="loadType('recent')"
-                    class="@if($type == 'recent') text-primary @else text-gray-500 hover:text-gray-700 @endif 
+                    class="@if($type == 'recent') text-primary @else text-gray-500 hover:text-gray-700 @endif
                     col-span-4 bg-white rounded-xl shadow p-4 flex items-center justify-center">
-                
+
                     <div class="rounded-full mr-2">
                         @svg('heroicon-o-clock', ['class' => 'w-5 h-5'])
                     </div>
@@ -26,9 +26,9 @@
                 </button>
 
                 <button wire:click="loadType('trending')"
-                    class="@if($type == 'trending') text-primary @else text-gray-500 hover:text-gray-700 @endif 
+                    class="@if($type == 'trending') text-primary @else text-gray-500 hover:text-gray-700 @endif
                     col-span-4 bg-white rounded-xl shadow p-4 flex items-center justify-center">
-                
+
                     <div class="rounded-full mr-2">
                         @svg('heroicon-o-fire', ['class' => 'w-5 h-5'])
                     </div>
@@ -38,7 +38,7 @@
 
                 <button disabled
                     class="relative opacity-50 text-gray-500 col-span-4 bg-white rounded-xl shadow p-4 flex items-center justify-center">
-                
+
                     <div class="rounded-full mr-2">
                         @svg('heroicon-o-chat-alt-2', ['class' => 'w-5 h-5'])
                     </div>
@@ -46,7 +46,7 @@
                     <span class="text-14">{{ translate('Most Answers') }}</span>
 
                     {{-- <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-purple-100 text-purple-800">
-                        {{ translate('Coming soon!') }} </span> 
+                        {{ translate('Coming soon!') }} </span>
                 </button>
             </nav>
         </div> --}}
@@ -62,7 +62,7 @@
         </livewire:feed.elements.feed-card-empty-state>
     </div>
 
-    <div wire:init="loadInit"> 
+    <div wire:init="loadInit">
         @if(!empty($my_new_post))
             <livewire:feed.elements.feed-card wire:key="activity_{{ time().'_'.$my_new_post->id }}" :item="$my_new_post">
             </livewire:feed.elements.feed-card>
@@ -88,7 +88,9 @@
             <div class="mb-3 bg-white px-4 py-6 shadow sm:p-6 sm:rounded-lg w-full">
                 {{ translate('You\'ve reached the end. Fancy Sharing something?') }}
             </div>
+            @auth
             <livewire:feed.elements.add-post></livewire:feed.elements.add-post>
+            @endauth
 
             @endif
         </div>
