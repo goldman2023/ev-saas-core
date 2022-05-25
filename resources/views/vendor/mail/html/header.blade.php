a6ccb007802aa5958619caf16758e8713cd90a58
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-05-25 13:23:16 +0000

    Site name instead of an image in mail header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,8 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (!empty(get_tenant_setting('site_logo')))
-<img src="{{ Storage::url(get_tenant_setting('site_logo')->file_name) }}" class="logo" alt="{{ get_tenant_setting('site_name') }}"height="50px">
+{{-- <img src="{{ Storage::url(get_tenant_setting('site_logo')->file_name) }}" class="logo" alt="{{ get_tenant_setting('site_name') }}"height="50px"> --}}
+{{ get_tenant_setting('site_name') }}
 @else
 {{ $slot }}
 @endif
