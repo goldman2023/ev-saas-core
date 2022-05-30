faac7ac8d16a5e4d629c00aa85d3b422d5431482
Eimantas <eim.kasp@gmail.com>
2022-05-31 02:59:33 +0300

    Update BootstrapThemeFunctions.php

--- app/Listeners/Tenancy/BootstrapThemeFunctions.php
+++ app/Listeners/Tenancy/BootstrapThemeFunctions.php
@@ -22,7 +22,7 @@ class BootstrapThemeFunctions
             // Check if ThemeFunctionsServiceProvider exists in `themes/{theme_name}/app/Providers/` and boot it
             if(file_exists($theme_functions_file_path)) {
                 require_once($theme_functions_file_path);
-                if(!class_exists(\ThemeFunctionsServiceProvider::class)) {
+                if(!class_exists('ThemeFunctionsServiceProvider')) {
                     App::register(\ThemeFunctionsServiceProvider::class);
                 }
             }
