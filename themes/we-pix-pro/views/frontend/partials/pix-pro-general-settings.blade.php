2431d016b4cdd4598ca3ecc29099398f61f0a5e3
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-05-30 22:31:48 +0000

    StripeService currency default to eur + Dataset Samples part 1

--- themes/we-pix-pro/views/frontend/partials/pix-pro-general-settings.blade.php
+++ themes/we-pix-pro/views/frontend/partials/pix-pro-general-settings.blade.php
@@ -104,3 +104,83 @@
     </div>
 </div>
 <!-- END Downloads -->
+
+
+<!-- Dataset Samples -->
+<div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5 sm:mt-5" x-data="{}">
+    <label class="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2">
+        {{ translate('Dataset Samples') }}
+    </label>
+
+    <div class="mt-1 sm:mt-0 sm:col-span-2">
+        <div class="w-full" x-data="{
+            limit: 9999,
+            template: {
+                'title': '',
+                'description': '',
+                'file_size': '',
+                'url': '',
+            },
+            count() {
+                if(settings.pix_pro_dataset_samples === undefined || settings.pix_pro_dataset_samples === null) {
+                    settings.pix_pro_dataset_samples = [{...this.template}];
+                }
+        
+                return settings.pix_pro_dataset_samples.length;
+            },
+            add() {
+                if(this.count() >= this.limit) {
+                    return;
+                }
+                
+                settings.pix_pro_dataset_samples.push({...this.template});
+            },
+            remove(index) {
+                settings.pix_pro_dataset_samples.splice(index, 1);
+            },
+         }"
+        >
+            <template x-if="count() >= 1">
+                <template x-for="(item, index) in settings.pix_pro_dataset_samples">
+                    <div class="w-full flex">
+                        <div class="grid grid-cols-12 gap-3" :class="{'mt-2': index > 0}">
+                            <input type="text" 
+                                    class="form-standard col-span-12 lg:col-span-3"
+                                    x-bind:placeholder="'{{ translate('Title') }}'"
+                                    x-model="item.title" />
+    
+                                <input type="text" 
+                                    class="form-standard col-span-12 lg:col-span-3"
+                                    x-bind:placeholder="'{{ translate('Description') }}'"
+                                    x-model="item.description" />
+    
+                                <input type="text" 
+                                    class="form-standard col-span-12 lg:col-span-3"
+                                    x-bind:placeholder="'{{ translate('File Size') }}'"
+                                    x-model="item.file_size" />
+    
+                                <input type="text" 
+                                    class="form-standard col-span-12 lg:col-span-3"
+                                    x-bind:placeholder="'{{ translate('URL') }}'"
+                                    x-model="item.url" />
+                        </div>
+
+                        <span class="ml-2 flex items-center cursor-pointer" @click="remove(index)">
+                            @svg('heroicon-o-trash', ['class' => 'w-5 h-5 text-danger'])
+                        </span>
+                    </div>
+                </template>
+            </template>
+        
+            <div href="javascript:;" class="btn-ghost !pl-0 !text-14 mt-1" @click="add()" x-show="count() < limit">
+                @svg('heroicon-o-plus', ['class' => 'h-3 w-3 mr-2'])
+                {{ translate('Add new') }}
+            </div>
+        
+            {{-- @if(!empty($field))
+                <x-system.invalid-msg field="{{ $field }}" class="mt-1"></x-system.invalid-msg>
+            @endif --}}
+        </div>
+    </div>
+</div>
+<!-- END Downloads -->
