b8e2dd5510b828d6482caab78046cdf086549f0d
Eimantas <eim.kasp@gmail.com>
2022-06-05 18:09:21 +0300

    Update show.blade.php

--- resources/views/frontend/we-quiz/show.blade.php
+++ resources/views/frontend/we-quiz/show.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ translate('Quiz Solving') }}</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
