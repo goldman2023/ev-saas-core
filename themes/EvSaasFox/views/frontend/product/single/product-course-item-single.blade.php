47ad293a3332f5513afe0c529a8811b6c649fcb8
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-06-07 13:12:31 +0000

    fix

--- themes/EvSaasFox/views/frontend/product/single/product-course-item-single.blade.php
+++ themes/EvSaasFox/views/frontend/product/single/product-course-item-single.blade.php
@@ -52,8 +52,6 @@
                         <iframe width='640' height='360' src='${this.course_item.content}' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
                     `;
                 }
-
-                console.log(this.video_data);
             }
         }
     }" x-init="init()">
