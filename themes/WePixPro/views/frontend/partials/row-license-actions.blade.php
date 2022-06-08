73311adf311348ee4a1bc09ccb9d979731386f14
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-06-08 13:31:59 +0000

    Fixing UserSubscriptions start/end date casts

--- themes/WePixPro/views/frontend/partials/row-license-actions.blade.php
+++ themes/WePixPro/views/frontend/partials/row-license-actions.blade.php
@@ -1,13 +1,13 @@
 <li>
     <button type="button" class="w-full  flex items-center px-3 py-3 pr-4 text-gray-900 text-14" 
-            @click="$dispatch('display-modal', {'id': 'pix-pro-generate-license', 'serial_number' : '{{ $license->serial_number }}'})"
+            @click="$dispatch('display-modal', {'id': 'pix-pro-generate-license', 'serial_number' : '{{ $license?->serial_number ?? '' }}'})"
     >
         @svg('heroicon-o-document-download', ['class' => 'w-[18px] h-[18px] mr-2']) 
         <span class="ml-2">{{ translate('Generate License') }}</span>
     </button>
 </li>
 
-@if(empty($license->data['hardware_id'] ?? null))
+@if(empty($license?->data['hardware_id'] ?? null))
     <li class="border-t border-gray-200">
         <button type="button" class="w-full flex items-center px-3 py-3 pr-4 text-gray-900 text-14 hover:bg-danger hover:text-white">
             @svg('heroicon-o-x', ['class' => 'w-[18px] h-[18px] mr-2']) 
