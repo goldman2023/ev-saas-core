764ac9878c4d6a1c4984a32a01395a116bb8dcf8
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-06-08 14:08:34 +0000

    Adding MySubscriptionsTable and moving logic from PlansTable to it (for subscriptions)

---
+++ app/Http/Livewire/Dashboard/Tables/MySubscriptionsTable.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard\Tables;
+
+use Carbon;
+use DB;
+use App\Events\Plans\PlanSubscriptionCancel;
+use App\Events\Plans\PlanSubscriptionRevive;
+use App\Enums\UserSubscriptionStatusEnum;
+use App\Facades\MyShop;
+use App\Models\Order;
+use App\Models\UserSubscription;
+use App\Traits\Livewire\DispatchSupport;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
+
+class MySubscriptionsTable extends DataTableComponent
+{
+    use DispatchSupport;
+
+    public ?int $searchFilterDebounce = 800;
+    public string $defaultSortColumn = 'created_at';
+    public string $defaultSortDirection = 'desc';
+    public bool $columnSelect = true;
+    public int $perPage = 10;
+    public array $perPageAccepted = [10, 25];
+
+    public array $filterNames = [
+        'status' => 'Status'
+    ];
+
+    public array $bulkActions = [
+
+    ];
+
+    protected string $pageName = 'user_subscriptions';
+    protected string $tableName = 'user_subscriptions';
+
+    public function mount() {
+        parent::mount();
+    }
+
+    public function filters(): array
+    {
+        return [
+            'status' => Filter::make('Status')
+                ->select(array_merge([
+                    '' => translate('All'),
+                ], UserSubscriptionStatusEnum::labels())),
+        ];
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('ID')
+                ->excludeFromSelectable(),
+            Column::make('Title')
+                ->sortable()
+                ->excludeFromSelectable(),
+            Column::make('Status', 'status')
+                ->excludeFromSelectable(),
+            Column::make('Payment status', 'payment_status')
+                ->excludeFromSelectable(),
+            Column::make('Amount', 'amount')
+                ->excludeFromSelectable(),
+            Column::make('Price', 'price')
+                ->excludeFromSelectable(),
+            Column::make('Starting', 'start_date')
+                ->excludeFromSelectable(),
+            Column::make('Ending', 'end_date')
+                ->excludeFromSelectable(),
+            Column::make('Actions')
+                ->excludeFromSelectable(),
+        ];
+    }
+
+    public function query(): Builder
+    {
+        return auth()->user()->plan_subscriptions()->getQuery()->with(['subject', 'order'])
+            // ->where('end_date', '>', now())->orWhere('end_date', null)
+            // ->when($this->getFilter('search'), fn ($query, $search) => $query->search($search))
+            ->when($this->getFilter('status'), fn ($query, $status) => $query->where('status', $status));
+      }
+
+    public function rowView(): string
+    {
+        return 'frontend.dashboard.user-subscriptions.row-subscription';
+    }
+
+    // public function cancelPlan($user_subscription_id) {
+
+    //     if($this->for === 'me') {
+    //         $plan_subscription = auth()->user()->plan_subscriptions->where('id', $user_subscription_id)->first();
+
+    //         try {
+    //             if(!empty($plan_subscription)) {
+    //                 PlanSubscriptionCancel::dispatch($plan_subscription);
+    //             } else {
+    //                 throw new \Exception('Cannot find a subscription with ID: '.$user_subscription_id);
+    //             }
+    //         } catch(\Exception $e) {
+    //             $this->inform(translate('Error: Cannot cancel a subscription...'), $e->getMessage(), 'fail');
+    //             return false;
+    //         }
+            
+    //         $end_date = Carbon::createFromTimestamp($plan_subscription->end_date)->format('d. M Y, H:i');
+    //         $this->inform(translate('Subscription plan successfully canceled!'), 'Have in mind that you can still use the plan before ending period: '.$end_date, 'success', 5000);
+    //     }
+    // }
+
+    // public function revivePlan($user_subscription_id) {
+
+    //     if($this->for === 'me') {
+    //         $plan_subscription = auth()->user()->plan_subscriptions->where('id', $user_subscription_id)->first();
+
+    //         try {
+    //             if(!empty($plan_subscription)) {
+    //                 PlanSubscriptionRevive::dispatch($plan_subscription);
+    //             } else {
+    //                 throw new \Exception('Cannot find a subscription with ID: '.$user_subscription_id);
+    //             }
+    //         } catch(\Exception $e) {
+    //             $this->inform(translate('Error: Cannot revive a subscription...'), $e->getMessage(), 'fail');
+    //             return false;
+    //         }
+            
+    //         $this->inform(translate('Subscription plan successfully revived!'), '', 'success', 5000);
+    //     }
+    // }
+}
--- themes/WePixPro/views/frontend/partials/pix-pro-licenses-table.blade.php
+++ themes/WePixPro/views/frontend/partials/pix-pro-licenses-table.blade.php
@@ -1,5 +1,8 @@
 <div class="w-full mb-7">
-    <h2 class="text-32 text-gray-700 font-semibold mb-1">{{ translate('Licenses')}}</h2>
+    <div class="flex justify-between items-center bg-white py-4 px-4 border border-gray-200 rounded-lg">
+        <h4 class="text-18 text-gray-900 font-semibold">{{ translate('Licenses') }}</h4>
+     </div>
+    {{-- <h2 class="text-32 text-gray-700 font-semibold mb-1">{{ translate('Licenses')}}</h2> --}}
 
     <livewire:dashboard.tables.licenses-table for="me" :show-search="false" :show-filters="false" :show-filter-dropdown="false" :show-per-page="false" :column-select="false"/>
 </div>
