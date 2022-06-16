f137fea8ebd1664a7d7bf49eed9f730ff5a528c5
Eimantas <eim.kasp@gmail.com>
2022-06-17 00:31:53 +0300

    Spatie Permissions management Implementation in Nova
    Instaling this package:
    https://github.com/itsmejoshua/novaspatiepermissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ use Spatie\Activitylog\Models\Activity;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Permission\Traits\HasRoles;
 use DB;
+use Laravel\Cashier\Billable;
 use Laravel\Nova\Auth\Impersonatable;
 
 class User extends Authenticatable implements MustVerifyEmail, Wallet, WalletFloat
@@ -41,6 +42,7 @@ class User extends Authenticatable implements MustVerifyEmail, Wallet, WalletFlo
     use SocialReactionsTrait;
     use SocialFollowingTrait;
     use Impersonatable;
+    use Billable;
 
     protected $casts = [
         'trial_ends_at' => 'datetime',
