df3c4312ec8ab71f16701a1d53a8035c2b5640d5
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-06-26 20:41:10 +0000

    Pixpro - customer dashboard dynamic stats fix

--- resources/views/components/dashboard/widgets/customer/dynamic-stats.blade.php
+++ resources/views/components/dashboard/widgets/customer/dynamic-stats.blade.php
@@ -6,53 +6,19 @@
       <div class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
         <dt>
           <div class="absolute bg-indigo-500 rounded-md p-3">
-            <!-- Heroicon name: outline/users -->
-            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-            </svg>
+            @svg('heroicon-o-users', ['class' => 'h-6 w-6 text-white'])
           </div>
-          <p class="ml-16 text-sm font-medium text-gray-500 truncate">Current Plan</p>
+          <p class="ml-16 text-sm font-medium text-gray-500 truncate">{{ translate('Current Plan') }}</p>
         </dt>
         <dd class="ml-16 pb-6 flex items-baseline sm:pb-7">
-          <p class="text-2xl font-semibold text-gray-900">SOLO+ </p>
+          <p class="text-2xl font-semibold text-gray-900">{{ $user_subscription?->subject?->name ?? '' }}</p>
           <p class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
-            <!-- Heroicon name: solid/arrow-sm-up -->
-
-            <span class="sr-only"> Increased by </span>
-            1 Seat
+            {{ $user_subscription->qty }} {{ translate('Seat') }}
           </p>
           <div class="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
             <div class="text-sm">
               <a href="{{ route('my.plans.management') }}" class="font-medium text-indigo-600 hover:text-indigo-500">
-                Manage Subscription
-                <span class="sr-only"> Total Subscribers stats</span></a>
-            </div>
-          </div>
-        </dd>
-      </div>
-
-      <div class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
-        <dt>
-          <div class="absolute bg-indigo-500 rounded-md p-3">
-            <!-- Heroicon name: outline/mail-open -->
-            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76" />
-            </svg>
-          </div>
-          <p class="ml-16 text-sm font-medium text-gray-500 truncate">Billing period</p>
-        </dt>
-        <dd class="ml-16 pb-6 flex items-baseline sm:pb-7">
-          <p class="text-2xl font-semibold text-gray-900">yearly</p>
-          <p class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
-            <!-- Heroicon name: solid/arrow-sm-up -->
-
-            <span class="sr-only"> Increased by </span>
-            Next invoice in 22 days
-          </p>
-          <div class="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
-            <div class="text-sm">
-              <a href="{{ route('my.orders.all') }}" class="font-medium text-indigo-600 hover:text-indigo-500">
-                Manage invoices
+                {{ translate('Manage Subscription') }}
               </a>
             </div>
           </div>
@@ -62,24 +28,66 @@
       <div class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
         <dt>
           <div class="absolute bg-indigo-500 rounded-md p-3">
-            <!-- Heroicon name: outline/cursor-click -->
-            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-            </svg>
+            @svg('heroicon-o-mail-open', ['class' => 'h-6 w-6 text-white'])
           </div>
-          <p class="ml-16 text-sm font-medium text-gray-500 truncate">Cost</p>
+          <p class="ml-16 text-sm font-medium text-gray-500 truncate">{{ translate('Billing period') }}</p>
         </dt>
         <dd class="ml-16 pb-6 flex items-baseline sm:pb-7">
-          <p class="text-2xl font-semibold text-gray-900">100€ / year</p>
-          <p class="ml-2 flex items-baseline text-sm font-semibold text-red-600">
+          <p class="text-2xl font-semibold text-gray-900">
+            @if(($order?->invoicing_period ?? null) === 'year')
+              {{ translate('Yearly') }}
+            @elseif(($order?->invoicing_period ?? null) === 'month')
+              {{ translate('Monthly') }}
+            @elseif(($order?->invoicing_period ?? null) === 'week')
+              {{ translate('Weekly') }}
+            @else
+              ?
+            @endif
+          </p>
+          @if(($order?->type ?? null) === 'subscription' && !empty($invoices?->first()?->end_date ?? null))
+            <p class="ml-2 flex items-baseline text-sm font-semibold text-green-600">
+              {{ translate('Next invoice in').' '.(\Carbon::createFromTimestamp($invoices?->first()?->end_date)?->diffInDays() ?? '?').' '.translate('days') }}
+            </p>
+          @endif
+          <div class="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
+            <div class="text-sm">
+              <a href="{{ route('my.orders.all') }}" class="font-medium text-indigo-600 hover:text-indigo-500">
+                {{ translate('Manage invoices') }}
+              </a>
+            </div>
+          </div>
+        </dd>
+      </div>
 
+      <div class="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden">
+        <dt>
+          <div class="absolute bg-indigo-500 rounded-md p-3">
+            @svg('heroicon-o-currency-euro', ['class' => 'h-6 w-6 text-white'])
+          </div>
+          <p class="ml-16 text-sm font-medium text-gray-500 truncate">{{ translate('Cost') }}</p>
+        </dt>
+        <dd class="ml-16 pb-6 flex items-baseline sm:pb-7">
+          <p class="text-2xl font-semibold text-gray-900">{{ \FX::formatPrice($order?->total_price ?? 0) }} / 
+            <span class="text-16">
+            @if(($order?->invoicing_period ?? null) === 'year')
+              {{ translate('Yearly') }}
+            @elseif(($order?->invoicing_period ?? null) === 'month')
+              {{ translate('Monthly') }}
+            @elseif(($order?->invoicing_period ?? null) === 'week')
+              {{ translate('Weekly') }}
+            @else
+              ?
+            @endif
+            </span>
+          </p>
+          <p class="ml-2 flex items-baseline text-sm font-semibold text-red-600">
 
           </p>
           <div class="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
             <div class="text-sm">
-              <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                Modify plan
-                <span class="sr-only"> Avg. Click Rate stats</span></a>
+              <a href="{{ route('my.plans.management') }}" class="font-medium text-indigo-600 hover:text-indigo-500">
+                {{ translate('Modify plan') }}
+              </a>
             </div>
           </div>
         </dd>
