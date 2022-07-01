7b800af287a6a8a9c6940993afdc84e6fbbbb4fe
Eimantas <eim.kasp@gmail.com>
2022-07-01 15:19:06 +0300

    Update header_01.blade.php

--- resources/views/components/tailwind-ui/headers/header_01.blade.php
+++ resources/views/components/tailwind-ui/headers/header_01.blade.php
@@ -44,7 +44,7 @@
                     </a>
                     @else
                     <button @click="$dispatch('display-flyout-panel', {'id': 'cart-panel'});" type="button"
-                        class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        class="p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                         @svg('heroicon-o-shopping-cart', ['class' => 'h-6 w-6'])
                     </button>
 
