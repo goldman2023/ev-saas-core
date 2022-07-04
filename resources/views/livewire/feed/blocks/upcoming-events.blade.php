ba4a42c6133d811d88da08ead5d7dcd56894bfa9
Eimantas <eim.kasp@gmail.com>
2022-07-05 02:26:13 +0300

    Remove unused components

--- resources/views/livewire/feed/blocks/upcoming-events.blade.php
+++ resources/views/livewire/feed/blocks/upcoming-events.blade.php
@@ -1,84 +1,86 @@
+@if(!empty($events))
 <div class="w-full bg-white rounded-xl shadow">
     <div class="w-full px-5 py-4 mb-5 flex justify-between border-b border-gray-200">
         <h5 class="text-14 font-semibold">{{ translate('Upcoming Events') }}</h5>
     </div>
 
     <div class="px-5 pb-4 flex flex-col">
-        @if(!empty($events))
-            @foreach($events as $key => $event)
-                <div class="w-full pb-3 @if($events->count()-1 !== $key) mb-4 border-b border-gray-20 @endif">
-                    <div class="flex space-x-4">
-                        <div class="flex-shrink-0">
-                            <div class="inline-block relative">
-                                <a href="{{ $event->getPermalink() }}">
-                                    <x-tenant.system.image
-                                        class="h-11 w-11 rounded-xl border-3 ring-2 ring-gray-200" fit="cover"
-                                        :image="$event->getThumbnail()">
-                                    </x-tenant.system.image>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="min-w-0 flex-1">
-                            <div class="w-full flex flex-row ">
-                                <div class="w-full flex flex-col pr-4">
-                                    <a href="{{ $event->getPermalink() }}" class="text-14 text-typ-2 font-medium block hover:underline mb-1 leading-tight">
-                                        {{ $event->name }}
-                                    </a>
-                                    <p class="text-12 text-typ-3 line-clamp-2 mb-2">
-                                        {{ $event->excerpt }}
-                                    </p>
-                                </div>
-                                <div class="flex-shrink-0 self-start flex">
-                                    <div class="relative inline-block text-left cursor-pointer">
-                                        @svg('heroicon-s-dots-horizontal', ['class' => 'text-gray-400 w-5 h-5'])
-                                    </div>
-                                </div>
-                            </div>
-                            
 
-                            <div class="w-full flex flex-col relative">
-                                <div class="absolute top-0 right-0 flex items-center mb-2">
-                                    @if($event->getCoreMeta('location_type') === 'remote')
-                                        <span class="badge border border-primary text-typ-2 !text-10">{{ translate('Remote') }}</span>
-                                    @else
-                                        <span class="badge border border-green-700 text-typ-2 !text-10">{{ translate('Offline') }}</span>
-                                    @endif
-                                </div>
-
-                                <div class="flex flex-col text-typ-2">
-                                    <div class="w-full flex">
-                                        @svg('heroicon-o-calendar', ['class' => 'h-4 h-5 mr-2 '])
-                                        <span class="text-14">{{ translate('Start') }}:</span>
-                                    </div>
-                                   
-                                    <time datetime="{{ $event->getCoreMeta('start_date') }}" class="pl-7 text-12">
-                                        {{ \Carbon::createFromTimestamp($event->getCoreMeta('start_date'))->format('d.m.Y H:i')  }}
-                                    </time>
-                                </div>
-
-                                @if($event->getCoreMeta('date_type') === 'range' 
-                                        && !empty($event->getCoreMeta('end_date')))
-                                    <div class="flex flex-col text-typ-2">
-                                        <div class="w-full flex">
-                                            @svg('heroicon-o-calendar', ['class' => 'h-4 h-5 mr-2 '])
-                                            <span class="text-14">{{ translate('End') }}:</span>
-                                        </div>
-                                    
-                                        <time datetime="{{ $event->getCoreMeta('end_date') }}" class="pl-7 text-12">
-                                            {{ \Carbon::createFromTimestamp($event->getCoreMeta('end_date'))->format('d.m.Y H:i')  }}
-                                        </time>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-                        
+        @foreach($events as $key => $event)
+        <div class="w-full pb-3 @if($events->count()-1 !== $key) mb-4 border-b border-gray-20 @endif">
+            <div class="flex space-x-4">
+                <div class="flex-shrink-0">
+                    <div class="inline-block relative">
+                        <a href="{{ $event->getPermalink() }}">
+                            <x-tenant.system.image class="h-11 w-11 rounded-xl border-3 ring-2 ring-gray-200"
+                                fit="cover" :image="$event->getThumbnail()">
+                            </x-tenant.system.image>
+                        </a>
                     </div>
                 </div>
-            @endforeach
+                <div class="min-w-0 flex-1">
+                    <div class="w-full flex flex-row ">
+                        <div class="w-full flex flex-col pr-4">
+                            <a href="{{ $event->getPermalink() }}"
+                                class="text-14 text-typ-2 font-medium block hover:underline mb-1 leading-tight">
+                                {{ $event->name }}
+                            </a>
+                            <p class="text-12 text-typ-3 line-clamp-2 mb-2">
+                                {{ $event->excerpt }}
+                            </p>
+                        </div>
+                        <div class="flex-shrink-0 self-start flex">
+                            <div class="relative inline-block text-left cursor-pointer">
+                                @svg('heroicon-s-dots-horizontal', ['class' => 'text-gray-400 w-5 h-5'])
+                            </div>
+                        </div>
+                    </div>
 
-        @else
-            {{-- TODO: Empty State --}}
-        @endif
+
+                    <div class="w-full flex flex-col relative">
+                        <div class="absolute top-0 right-0 flex items-center mb-2">
+                            @if($event->getCoreMeta('location_type') === 'remote')
+                            <span class="badge border border-primary text-typ-2 !text-10">{{ translate('Remote')
+                                }}</span>
+                            @else
+                            <span class="badge border border-green-700 text-typ-2 !text-10">{{ translate('Offline')
+                                }}</span>
+                            @endif
+                        </div>
+
+                        <div class="flex flex-col text-typ-2">
+                            <div class="w-full flex">
+                                @svg('heroicon-o-calendar', ['class' => 'h-4 h-5 mr-2 '])
+                                <span class="text-14">{{ translate('Start') }}:</span>
+                            </div>
+
+                            <time datetime="{{ $event->getCoreMeta('start_date') }}" class="pl-7 text-12">
+                                {{ \Carbon::createFromTimestamp($event->getCoreMeta('start_date'))->format('d.m.Y H:i')
+                                }}
+                            </time>
+                        </div>
+
+                        @if($event->getCoreMeta('date_type') === 'range'
+                        && !empty($event->getCoreMeta('end_date')))
+                        <div class="flex flex-col text-typ-2">
+                            <div class="w-full flex">
+                                @svg('heroicon-o-calendar', ['class' => 'h-4 h-5 mr-2 '])
+                                <span class="text-14">{{ translate('End') }}:</span>
+                            </div>
+
+                            <time datetime="{{ $event->getCoreMeta('end_date') }}" class="pl-7 text-12">
+                                {{ \Carbon::createFromTimestamp($event->getCoreMeta('end_date'))->format('d.m.Y H:i') }}
+                            </time>
+                        </div>
+                        @endif
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        @endforeach
+
+
 
         <div class="w-full border-t border-gray-200 flex justify-center">
             <a href="#" class="text-typ-3 hover:underline w-full pt-2 text-14 text-center">
@@ -87,3 +89,6 @@
         </div>
     </div>
 </div>
+@else
+{{-- TODO: Empty State --}}
+@endif
--- themes/EvTailwind/views/frontend/layouts/feed.blade.php
+++ themes/EvTailwind/views/frontend/layouts/feed.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="min-h-full bg-gray-200">
-    <div class="max-w-5xl mx-auto  px-1 sm:px-6 lg:max-w-[1280px] lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8 py-10 pt-3">
+    <div class="max-w-5xl mx-auto  px-2 md:px-6 lg:max-w-[1280px] lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8 py-10 pt-3">
         <div class="hidden lg:block lg:col-span-2 sm:col-span-2">
             <x-feed.elements.feed-sidebar>
             </x-feed.elements.feed-sidebar>
         </div>
-        <main class="sm:col-span-10 grid grid-cols-12 gap-6">
+        <main class="sm:col-span-10 grid grid-cols-12 md:gap-6">
             @yield('feed_content')
         </main>
     </div>
