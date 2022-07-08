6be6f3a8ad9ef695bcc10e22642685839ab598e4
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-08 09:51:56 +0000

    added business profile form for settings

---
+++ app/Http/Livewire/Dashboard/Forms/Settings/BusinessProfileForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard\Forms\Settings;
+
+use Livewire\Component;
+
+class BusinessProfileForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.forms.settings.business-profile-form');
+    }
+}
