bf55ced9b2ba55bede9a8ee724aa8fa653725d65
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-11 13:05:05 +0000

    added mount function and necessary imports

--- app/Http/Livewire/Dashboard/Forms/Settings/BusinessProfileForm.php
+++ app/Http/Livewire/Dashboard/Forms/Settings/BusinessProfileForm.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Livewire\Dashboard\Forms\Settings;
 
+use Spatie\ValidationRules\Rules\ModelsExist;
 use Livewire\Component;
+use App\Traits\Livewire\RulesSets;
+use Illuminate\Support\Facades\Http;
+use TenantSettings;
+use App\Models\TenantSetting;
+use App\Traits\Livewire\DispatchSupport;
+use Illuminate\Validation\Rule;
+use DB;
 
 class BusinessProfileForm extends Component
 {
+    public $settings;
+
+    public function mount(){
+        $this->settings = TenantSettings::getAll();
+
+    }
+
     public function render()
     {
         return view('livewire.dashboard.forms.settings.business-profile-form');
