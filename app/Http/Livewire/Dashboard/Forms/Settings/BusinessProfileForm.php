c4bb37999429a4b3738b254a52dfabc216336922
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-13 05:01:55 +0000

    adding functions to save different sections and debugging

--- app/Http/Livewire/Dashboard/Forms/Settings/BusinessProfileForm.php
+++ app/Http/Livewire/Dashboard/Forms/Settings/BusinessProfileForm.php
@@ -38,9 +38,10 @@ class BusinessProfileForm extends Component
             'basic' => [
                 'settings.*' => [],
                 'business.thumbnail' => ['sometimes', 'if_id_exists:App\Models\Upload,id,true'],
-                'business.cover' => ['sometimes', 'if_id_exists:App\Models\Upload,id,true'],
+                'business.cover' => ['sometimes','if_id_exists:app\models\upload,id,true'],
                 'business.name' => ['required', 'min:3'],
                 'business.excerpt' => ['required', 'min:30'],
+                'business.content' => ['required', 'min:30'],
                 'settings.tagline' => ['required'],
                 'settings.phones' => ['required'],
                 'settings.email' => ['required','email:rfs','dns'], 
@@ -94,5 +95,151 @@ class BusinessProfileForm extends Component
     public function render()
     {
         return view('livewire.dashboard.forms.settings.business-profile-form');
+    }
+    
+    public function saveBasicInformation()
+    {
+        $rules = $this->getRuleSet('basic');
+
+        try {
+            $this->validate($rules);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            $this->dispatchValidationErrors($e);
+            $this->validate($rules);
+        }
+
+        $this->business->offsetUnset('pivot'); 
+
+        DB::beginTransaction();
+
+        try {
+            $this->business->syncUploads();
+            // $this->business->content = Purifier::clean($this->business->content); // TODO: Fix purifier to prevent XSS
+
+            $this->business->save();
+
+            $this->saveSettings($rules);
+
+            DB::commit();
+
+            $this->inform(translate('Basic business information successfully saved.'), '', 'success');
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            $this->inform(translate('Could not save basic business information.'), $e->getMessage(), 'fail');
+        }
+    }
+
+    public function saveCompanyInfo()
+    {
+        $rules = $this->getRuleSet('company_info');
+
+        try {
+            $this->validate($rules);
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            $this->dispatchValidationErrors($e);
+            $this->validate($rules);
+        }
+
+        try {
+            $this->saveSettings($rules);
+
+            DB::commit();
+
+            $this->inform(translate('Company information successfully saved.'), '', 'success');
+        } catch (\Exception $e) {
+            DB::rollback();
+            $this->inform(translate('Could not save company information.'), $e->getMessage(), 'fail');
+        }
+    }
+
+    public function saveContactDetails($contacts, $current = null)
+    {
+        if ($current['is_primary'] ?? null) {
+            $contacts = collect($contacts)->map(function ($item) use ($current) {
+                if ($item['email'] == $current['email'] && $item['department_name'] == $current['department_name']) {
+                    $item['is_primary'] = true;
+                } else {
+                    $item['is_primary'] = false;
+                }
+
+                return $item;
+            })->toArray();
+        }
+
+        $contact_details = $this->business->settings()->get()->keyBy('setting')->get('contact_details');
+
+        if (empty($contact_details)) {
+            $contact_details = new ShopSetting();
+            $contact_details->business_id = $this->business->id;
+            $contact_details->setting = 'contact_details';
+        }
+
+        $contact_details->value = json_encode($contacts);
+        $contact_details->save();
+
+        $this->settings['contact_details'] = $contacts;
+
+        // $this->dispatchBrowserEvent('contact-details-modal-hide');
+        $this->dispatchBrowserEvent('toggle-flyout-panel', ['id' => 'contact-panel', 'timeout' => '500']);
+        $this->inform(translate('Contact details successfully updated.'), '', 'success');
+    }
+
+    public function removeContactDetails($contacts, $current = null)
+    {
+        $contact_details = $this->business->settings->keyBy('setting')->get('contact_details');
+
+        foreach ($contacts as $key => $contact) {
+            if ($contact == $current) {
+                unset($contacts[$key]);
+            }
+        }
+
+        $contacts = array_values($contacts);
+
+        $has_primary = false;
+        foreach ($contacts as $key => $contact) {
+            if ($contact['is_primary'] ?? false) {
+                $has_primary = true;
+            }
+        }
+
+        if (! $has_primary && isset($contacts[0])) {
+            $contacts[0]['is_primary'] = true;
+        }
+
+        $contact_details->value = json_encode(array_values($contacts));
+        $contact_details->save();
+
+        $this->settings['contact_details'] = $contact_details->value;
+
+        $this->dispatchBrowserEvent('contact-details-modal-hide');
+        $this->toastify(translate('Contact details successfully removed.', 'success'));
+    }
+
+    /*
+     * Saves all settings provided in $rules variable.
+     */
+    protected function saveSettings($rules)
+    {
+        foreach (collect($rules)->filter(fn ($item, $key) => str_starts_with($key, 'settings')) as $key => $value) {
+            $setting_key = explode('.', $key)[1]; // get the part after `core_meta.`
+
+            if (! empty($setting_key) && $setting_key !== '*') {
+                $new_value = castValueForSave($setting_key, $this->settings[$setting_key], ShopSetting::metaDataTypes());
+
+                try {
+                    ShopSetting::updateOrCreate(
+                        [
+                            'shop_id' => $this->business->id, 
+                            'setting' => $setting_key, 
+                        ],
+                        ['value' => $new_value]
+                    );
+                } catch(\Exception $e) {
+                    Log::error($e->getMessage());
+                }
+            }
+        }
     }
 }
