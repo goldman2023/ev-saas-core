f780d0383a09037c0ace1fa55f0e1fae8a32706e
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-18 11:15:56 +0000

    added neccessary import and uplaoding function

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,10 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use App\Traits\UploadTrait;
+use App\Traits\CoreMetaTrait;
+use App\Traits\GalleryTrait;
+use Spatie\Sluggable\HasSlug;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
     use HasFactory;
+    use UploadTrait;
+    use GalleryTrait;
+    use CoreMetaTrait;
+    use SoftDeletes;
+    use HasSlug;
+    use LogsActivity;
 
     protected $table = 'tasks';
 
@@ -22,5 +34,16 @@ class Task extends Model
    
     public function subject(){
         return $this->morphTo('subject');
+    }
+
+    public function getDynamicModelUploadProperties(): array
+    {
+        return [
+            [
+                'property_name' => 'documents',
+                'relation_type' => 'documents',
+                'multiple' => true,
+            ]
+        ];
     }
 }
