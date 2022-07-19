3acb85a9144ae503e805cece23a83df027120d54
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-19 13:46:08 +0000

    edit view

--- resources/views/frontend/dashboard/tasks/edit.blade.php
+++ resources/views/frontend/dashboard/tasks/edit.blade.php
@@ -12,7 +12,7 @@
         <x-dashboard.section-headers.section-header title="{{ translate('Edit Task') }}" text="">
             <x-slot name="content">
                 <a href="{{ route('tasks.index') }}" class="btn-standard">
-                    @svg('heroicon-o-chart-pie', ['class' => 'h-4 h-4 mr-2'])
+                    @svg('heroicon-o-chevron-left', ['class' => 'h-4 h-4 mr-2'])
                     <span>{{ translate('All Tasks') }}</span>
                 </a>
             </x-slot>
