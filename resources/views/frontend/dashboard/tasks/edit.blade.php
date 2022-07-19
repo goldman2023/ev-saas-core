08a9c587686dd55566e248780867ae8a083896b3
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-19 12:20:27 +0000

    tasks edit view

---
+++ resources/views/frontend/dashboard/tasks/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('frontend.layouts.user_panel')
+
+@section('page_title', translate('Edit Task'))
+
+@push('head_scripts')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.11/themes/airbnb.min.css">
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+@endpush
+
+@section('panel_content')
+    <section>
+        <x-dashboard.section-headers.section-header title="{{ translate('Edit Task') }}" text="">
+            <x-slot name="content">
+                <a href="{{ route('tasks.index') }}" class="btn-standard">
+                    @svg('heroicon-o-chart-pie', ['class' => 'h-4 h-4 mr-2'])
+                    <span>{{ translate('All Tasks') }}</span>
+                </a>
+            </x-slot>
+        </x-dashboard.section-headers.section-header>
+
+        <livewire:dashboard.forms.tasks.task-form :task="$task"></livewire:dashboard.forms.tasks.task-form>
+    </section>
+@endsection
+
+@push('footer_scripts')
+@endpush
