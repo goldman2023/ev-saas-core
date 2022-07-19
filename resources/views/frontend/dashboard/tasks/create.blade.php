46e37a459d9abd0ff240486443c86b14705e8be8
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-19 12:19:56 +0000

    tasks create view

--- resources/views/frontend/dashboard/tasks/create.blade.php
+++ resources/views/frontend/dashboard/tasks/create.blade.php
@@ -1,24 +1,24 @@
 @extends('frontend.layouts.user_panel')
 
-@section('page_title', translate('All Products'))
+@section('page_title', translate('All Tasks'))
 
 @push('head_scripts')
-
+<link href="https://cdn.jsdelivr.net/npm/froala-editor@latest/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@latest/js/froala_editor.pkgd.min.js"></script>
 @endpush
 
 @section('panel_content')
-    <!-- Card -->
-    <div class="card">
-        <!-- Card Body -->
-        <div class="card-body">
-            <div class="row align-items-center">
-                <div class="col-sm mb-2 mb-sm-0">
-
-
-                </div>
-            </div>
-        </div>
-    </div>
+    <section>
+        <x-dashboard.section-headers.section-header title="{{ translate('New task') }}" text="">
+            <x-slot name="content">
+                <a href="{{ route('tasks.index') }}" class="btn-standard">
+                    @svg('heroicon-o-chevron-left', ['class' => 'h-4 h-4 mr-2'])
+                    <span>{{ translate('All tasks') }}</span>
+                </a>
+            </x-slot>
+        </x-dashboard.section-headers.section-header>
+        <livewire:dashboard.forms.tasks.task-form ></livewire:dashboard.forms.tasks.task-form>
+    </section>
 @endsection
 
 @push('footer_scripts')
