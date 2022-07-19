2cde93341ac4a50c221813cd305ff2a7b730812e
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-19 14:30:37 +0000

    adding the table

--- app/Http/Livewire/Dashboard/Tables/TasksTable.php
+++ app/Http/Livewire/Dashboard/Tables/TasksTable.php
@@ -3,11 +3,87 @@
 namespace App\Http\Livewire\Dashboard\Tables;
 
 use Livewire\Component;
+use DB;
+use Carbon;
+use App\Enums\TaskTypesEnum;
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
 
 class TasksTable extends Component
 {
-    public function render()
+    public ?int $searchFilterDebounce = 800;
+
+    public string $defaultSortColumn = 'created_at';
+
+    public string $defaultSortDirection = 'desc';
+
+    public bool $columnSelect = true;
+
+    public int $perPage = 25;
+
+    public array $perPageAccepted = [10, 25, 50, 100];
+
+    protected string $pageName = 'tasks';
+
+    protected string $tableName = 'tasks';
+
+
+    public function exportSelected()
     {
-        return view('livewire.dashboard.tables.tasks-table');
+    }
+
+    public function filters(): array
+    {
+        return [
+            'status' => Filter::make('Status')
+                ->select([
+                    '' => translate('All'),
+                    TaskTypesEnum::issue()->value => translate('Issue'),
+                    TaskTypesEnum::improvement()->value => translate('Improvement'),
+                    TaskTypesEnum::other()->value => translate('Other'),
+                    TaskTypesEnum::payment()->value => translate('Payment'),
+                    TaskTypesEnum::request()->value => translate('Request'),
+                ]),
+        ];
+    }
+
+    public function query(): Builder
+    {
+
+        return Task::query()
+            ->when($this->getFilter('search'), fn ($query, $search) => $query->search($search))
+            ->when($this->getFilter('status'), fn ($query, $status) => $query->where('status', $status))
+            ->when($this->getFilter('type'), fn ($query, $type) => $query->where('type', $type));
+    }
+    public function columns(): array
+    {
+        return [
+            Column::make('ID','id')
+                ->sortable()
+                ->excludeFromSelectable(),
+            Column::make('Name', 'name')
+                ->excludeFromSelectable()
+                ->addClass('hidden md:table-cell'),
+            Column::make('Assigne', 'assignee_id')
+                ->excludeFromSelectable()
+                ->addClass('hidden md:table-cell'),
+            Column::make('Status', 'status')
+                ->excludeFromSelectable(),
+            Column::make('Type', 'type')
+                ->excludeFromSelectable(),
+            Column::make('Date', 'created_at')
+                ->excludeFromSelectable()
+                ->sortable(),
+            Column::make('Actions')
+                ->excludeFromSelectable(),
+        ];
+    }
+    public function rowView(): string
+    {
+        return 'frontend.dashboard.tasks.row';
     }
 }
+
