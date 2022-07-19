9cc48e81d17c053a66cbf857a781f7a9c56387ea
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-19 13:46:26 +0000

    tasks table for index view

---
+++ app/Http/Livewire/Dashboard/Tables/TasksTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dashboard\Tables;
+
+use Livewire\Component;
+
+class TasksTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.tables.tasks-table');
+    }
+}
