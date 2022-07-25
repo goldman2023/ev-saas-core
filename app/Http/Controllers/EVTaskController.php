77197e8110cb043d36ee162a9573e88b63b6f537
Stephen Adesina <bauerstevenson@gmail.com>
2022-07-25 10:58:37 +0000

    added destroy and completed functions

--- app/Http/Controllers/EVTaskController.php
+++ app/Http/Controllers/EVTaskController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use MyShop;
+use DB;
 
 class EVTaskController extends Controller
 {
+
+
 
     public function index(Request $request){
         $tasks = Task::all();
@@ -25,12 +28,23 @@ class EVTaskController extends Controller
     }
 
     public function destroy(Request $request, $id){
-
-        return view('frontend.dashboard.tasks.destroy');
+        $is_deleted = DB::delete('delete from tasks where id = ?',[$id]);
+        if($is_deleted){
+            return redirect()->route('tasks.index');
+        }
+        else{
+            $this->inform(translate('There was an error deleting a task...Please try again.'), '', 'fail');
+        }
     }
     
     public function details(Request $request, $id){
         $task = Task::findOrFail($id);
         return view('frontend.dashboard.tasks.details',compact('task'));
+    }
+
+    public function completed(Request $request, $id){
+
+        DB::update('update tasks set status="done" where id = ?',[$id]);
+        return back();
     }
 }
