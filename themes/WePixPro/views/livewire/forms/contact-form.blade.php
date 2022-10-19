5330b49515db0df6b3339498bfd574fa1ce1fcb4
Eimantas <eim.kasp@gmail.com>
2022-10-19 15:44:30 +0300

    Update contact-form.blade.php

--- themes/WePixPro/views/livewire/forms/contact-form.blade.php
+++ themes/WePixPro/views/livewire/forms/contact-form.blade.php
@@ -23,11 +23,13 @@
     </div>
     {{-- <div>
         <div class="flex justify-between">
-        <label for="phone" class="block text-sm font-medium text-gray-900">Phone</label>
-        <span id="phone-optional" class="text-sm text-gray-500">Optional</span>
+            <label for="phone" class="block text-sm font-medium text-gray-900">Phone</label>
+            <span id="phone-optional" class="text-sm text-gray-500">Optional</span>
         </div>
         <div class="mt-1">
-            <input type="text" name="phone" id="phone" autocomplete="tel" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" aria-describedby="phone-optional">
+            <input type="text" name="phone" id="phone" autocomplete="tel"
+                class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                aria-describedby="phone-optional">
         </div>
     </div> --}}
     <div class="sm:col-span-2">
@@ -42,25 +44,32 @@
             {{-- <span id="message-max" class="text-sm text-gray-500">Max. 500 characters</span> --}}
         </div>
         <div class="mt-1">
-            <textarea wire:model.defer="message" rows="4" class="@error('message') is-invalid @enderror py-3 px-4 block w-full shadow-sm text-gray-900 border border-gray-300 rounded-md"></textarea>
+            <textarea wire:model.defer="message" rows="4"
+                class="@error('message') is-invalid @enderror py-3 px-4 block w-full shadow-sm text-gray-900 border border-gray-300 rounded-md"></textarea>
 
             <x-system.invalid-msg field="message"></x-system.invalid-msg>
         </div>
     </div>
     <div class="sm:col-span-2">
         <div class="flex items-start">
-          <div class="flex-shrink-0">
-            <button type="button" @click="consent = !consent" :class="{'bg-primary':consent,'bg-gray-200':!consent}" class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary" role="switch" aria-checked="false">
-              <span class="sr-only">Agree to policies</span>
-              <span aria-hidden="true" :class="{'translate-x-5':consent,'translate-x-0':!consent}" class="inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
-            </button>
-          </div>
-          <div class="ml-3">
-            <p class="text-base text-gray-500 @error('consent') text-danger @enderror cursor-pointer" @click="consent = !consent" >
-              {{ translate('I have read and accept the') }}
-              <a href="#" class="font-medium text-primary underline">{{ translate('Privacy Policy') }}</a>
-            </p>
-          </div>
+            <div class="flex-shrink-0">
+                <button type="button" @click="consent = !consent" :class="{'bg-primary':consent,'bg-gray-200':!consent}"
+                    class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+                    role="switch" aria-checked="false">
+                    <span class="sr-only">Agree to policies</span>
+                    <span aria-hidden="true" :class="{'translate-x-5':consent,'translate-x-0':!consent}"
+                        class="inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"></span>
+                </button>
+            </div>
+            <div class="ml-3">
+                <p class="text-base text-gray-500 @error('consent') text-danger @enderror cursor-pointer">
+                    <span @click="consent = !consent">
+                        {{ translate('I have read and accept the') }}
+                    </span>
+                    <a href="/privacy-policy-page/" target="_blank" class="font-medium text-primary underline">{{
+                        translate('Privacy Policy') }}</a>
+                </p>
+            </div>
         </div>
 
         <x-system.invalid-msg field="consent"></x-system.invalid-msg>
