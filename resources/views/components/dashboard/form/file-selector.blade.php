70e688de67987cd4a6dac65b73918e23ec86218d
Vukasin Jockovic <jockovicvukasin@gmail.com>
2022-11-05 14:48:08 +0000

    Fix meta-fields file-selector

--- resources/views/components/dashboard/form/file-selector.blade.php
+++ resources/views/components/dashboard/form/file-selector.blade.php
@@ -171,13 +171,12 @@ x-init="makeSortable()"
                         <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                             <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" />
                         </svg>
-                        <div class="flex text-sm text-gray-600">
+                        <div class="flex justify-center text-sm text-gray-600">
                             <label class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                                 <span>{{ translate('Select a file') }}</span>
                             </label>
-                            <p class="pl-1">{{ translate('or drag and drop') }}</p>
                         </div>
-                        <p class="text-xs text-gray-500">{{ translate('PNG, JPG, GIF up to 3MB') }}</p>
+                        <p class="text-xs text-gray-500">{{ translate('PNG, JPG, GIF') }}</p>
                     </div>
                 </template>
             @else
