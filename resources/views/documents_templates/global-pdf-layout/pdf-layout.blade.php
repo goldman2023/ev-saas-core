272aa14e0ac38b77975a66432a39bcf480b51616
Eimantas <eim.kasp@gmail.com>
2022-12-02 16:04:21 +0200

    Update pdf templates to use pdf-layout

--- resources/views/documents_templates/global-pdf-layout/pdf-layout.blade.php
+++ resources/views/documents_templates/global-pdf-layout/pdf-layout.blade.php
@@ -1,17 +1,40 @@
-
 <html>
-    <head>
-        <style>
-            .strong {
-                font-weight: 700;
-            }
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        .strong {
+            font-weight: 700;
+        }
 
-            .center {
-                text-align: center
-            }
-        </style>
-    </head>
-    <body>
-        @yield('content')
-    </body>
+        .center {
+            text-align: center
+        }
+        .page-break {
+            page-break-after: always;
+        }
+
+        .text-center {
+            text-align: center;
+            width: 100%;
+        }
+
+        .watermark {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background: url('{{ get_site_logo() }}') no-repeat;
+            opacity: 0.5;
+            background-size: contain;
+            background-position: center center;
+            z-index: -1;
+        }
+    </style>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
 </html>
