c7a098b847ea75ddb84c5faf13bae70957e5dca5
Vukasin Jockovic <jockovicvukasin@gmail.com>
2023-01-06 19:15:56 +0000

    [PixPro Task] After activating license (adding HW) - refresh licenses table (to show hw_id) and download NON-EMPTY .data file!

--- themes/WePixPro/App/Http/Livewire/Dashboard/Forms/GenerateLicenseForm.php
+++ themes/WePixPro/App/Http/Livewire/Dashboard/Forms/GenerateLicenseForm.php
@@ -62,19 +62,20 @@ class GenerateLicenseForm extends Component
         try {
             $response = pix_pro_activate_license($license->user, $this->serial_number, $this->hw_id);
 
-            if(!empty($response) && !empty($response['status'] ?? null) && $response['status'] === 'success') {
-                $license->data = array_merge($license->data, [
+            if(!empty($response) && ($response['status'] ?? null) === 'success' && ($response['license_file']['status'] ?? null) === 'success') {
+                $license->mergeData([
                     'file_name' => $response['license_file']['file_name'] ?? '',
-                    'file_contents' => $response['license_file']['file_contents'] ?? ''
+                    'file_contents' => $response['license_file']['file_contents'] ?? '',
+                    'hardware_id' => $response['license']['hardware_id'] ?? $this->hw_id,
                 ]);
                 $license->save();
 
                 $this->emit('refreshDatatable');
                 $this->inform(translate('You successfully activated your license!'), translate('Serial number: ').$this->serial_number, 'success');
 
-                return response()->streamDownload(function () {
-                    echo $response['license_file']['file_contents'] ?? '';
-                }, $response['license_file']['file_name'] ?? '');
+                return response()->streamDownload(function () use($license) {
+                    echo $license->getData('file_contents');
+                }, $license->getData('file_name'));
             }
 
             $this->inform(translate('Cannot activate the license(invalid Hardware ID) OR license is already activated...'), translate('Serial number: ').$this->serial_number, 'fail');
