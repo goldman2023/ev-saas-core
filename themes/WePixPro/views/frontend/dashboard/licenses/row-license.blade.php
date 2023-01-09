eb850370567b079571c3f398a141f1bb9936f3de
Vukasin Jockovic <jockovicvukasin@gmail.com>
2023-01-09 16:48:59 +0000

    [Pix] Show licenses action dropdown if subscription is Active OR if license expiration_date is in future

--- themes/WePixPro/views/frontend/dashboard/licenses/row-license.blade.php
+++ themes/WePixPro/views/frontend/dashboard/licenses/row-license.blade.php
@@ -54,24 +54,24 @@
 
 <x-livewire-tables::table.cell class="align-middle  text-center">
     @isset($row->user_subscription)
-    @if(!empty($row->user_subscription->first()?->end_date ?? false))
-    @if($row->user_subscription->first()->end_date < now())
-    <span class="text-red-600">  Expired:
-    @endif
+        @if(!empty($row->user_subscription->first()?->end_date ?? false))
+            @if($row->user_subscription->first()->end_date < now())
+                <span class="text-red-600">  Expired:
+            @endif
 
-        {{ $row->user_subscription->first()->end_date->format('d. M Y, H:i') }}
+                {{ $row->user_subscription->first()->end_date->format('d. M Y, H:i') }}
 
-        @if($row->user_subscription->first()->end_date < now())
-    </span>
-    @endif
+            @if($row->user_subscription->first()->end_date < now())
+                </span>
+            @endif
     @elseif(!empty($row->getData('expiration_date')))
         @if($row->getData('expiration_date') < now())
-        <span class="text-red-600"> Expired:
+            <span class="text-red-600"> Expired:
         @endif
-        {{ \Carbon::createFromFormat('Y-m-d H:i:s', $row->getData('expiration_date'))->format('d. M Y, H:i') }}
+            {{ \Carbon::createFromFormat('Y-m-d H:i:s', $row->getData('expiration_date'))->format('d. M Y, H:i') }}
 
         @if($row->getData('expiration_date') < now())
-        </span>
+            </span>
         @endif
     @else
         -
@@ -88,36 +88,45 @@
         >
             @svg('heroicon-o-chevron-down', ['class' => 'w-[18px] h-[18px]'])
         </button>
-        <ul x-show="isOpen"
-            @click.away="isOpen = false"
-            class="absolute bg-white z-10 right-0 list-none p-0 border rounded mt-10 shadow overflow-hidden"
-        >
-            @if(empty($row?->data['hardware_id'] ?? null))
-                <li>
-                    <button type="button" class="w-full flex items-center px-3 py-3 pr-4 text-gray-900 text-14 hover:bg-primary hover:text-white"
-                        @click="$dispatch('display-modal', {'id': 'pix-pro-generate-license', 'serial_number' : '{{ $row?->serial_number ?? '' }}', 'license_id': '{{ $row->id ?? '' }}'})">
-                        @svg('heroicon-o-document-arrow-down', ['class' => 'w-[18px] h-[18px] mr-2'])
-                        <span class="ml-2">{{ translate('Generate License') }}</span>
-                    </button>
-                </li>
-            @endif
-            
-            @if(!empty($row?->data['hardware_id'] ?? null))
-                <li class="border-t border-gray-200">
-                    <button type="button" wire:click="downloadLicense({{ $row->id }})"
-                        class="w-full flex items-center px-3 py-3 pr-4 text-gray-900 text-14 hover:bg-danger hover:text-white">
-                        @svg('heroicon-o-document-arrow-down', ['class' => 'w-[18px] h-[18px] mr-2'])
-                        <span class="ml-2">{{ translate('Download .DAT file') }}</span>
-                    </button>
-                </li>
-                <li class="border-t border-gray-200">
-                    <button type="button" wire:click="disconnect({{ $row->id }})"
-                        class="w-full flex items-center px-3 py-3 pr-4 text-gray-900 text-14 hover:bg-danger hover:text-white">
-                        @svg('heroicon-o-x-mark', ['class' => 'w-[18px] h-[18px] mr-2'])
-                        <span class="ml-2">{{ translate('Deactivate') }}</span>
-                    </button>
-                </li>
-            @endif
-        </ul>
+
+        {{-- show actions ony if: 1) subscription is active OR license expiration_date is in future --}}
+        @if(
+            $row->user_subscription->first()->isActive()
+            ||
+            (!empty($row->getData('expiration_date')) && $row->getData('expiration_date') > now())
+        )
+            <ul x-show="isOpen"
+                @click.away="isOpen = false"
+                class="absolute bg-white z-10 right-0 list-none p-0 border rounded mt-10 shadow overflow-hidden"
+            >
+                @if(empty($row?->data['hardware_id'] ?? null))
+                    <li>
+                        <button type="button" class="w-full flex items-center px-3 py-3 pr-4 text-gray-900 text-14 hover:bg-primary hover:text-white"
+                            @click="$dispatch('display-modal', {'id': 'pix-pro-generate-license', 'serial_number' : '{{ $row?->serial_number ?? '' }}', 'license_id': '{{ $row->id ?? '' }}'})">
+                            @svg('heroicon-o-document-arrow-down', ['class' => 'w-[18px] h-[18px] mr-2'])
+                            <span class="ml-2">{{ translate('Generate License') }}</span>
+                        </button>
+                    </li>
+                @endif
+                
+                @if(!empty($row?->data['hardware_id'] ?? null))
+                    <li class="border-t border-gray-200">
+                        <button type="button" wire:click="downloadLicense({{ $row->id }})"
+                            class="w-full flex items-center px-3 py-3 pr-4 text-gray-900 text-14 hover:bg-danger hover:text-white">
+                            @svg('heroicon-o-document-arrow-down', ['class' => 'w-[18px] h-[18px] mr-2'])
+                            <span class="ml-2">{{ translate('Download .DAT file') }}</span>
+                        </button>
+                    </li>
+                    <li class="border-t border-gray-200">
+                        <button type="button" wire:click="disconnect({{ $row->id }})"
+                            class="w-full flex items-center px-3 py-3 pr-4 text-gray-900 text-14 hover:bg-danger hover:text-white">
+                            @svg('heroicon-o-x-mark', ['class' => 'w-[18px] h-[18px] mr-2'])
+                            <span class="ml-2">{{ translate('Deactivate') }}</span>
+                        </button>
+                    </li>
+                @endif
+            </ul>
+        @endif
+        
     </div>
 </x-livewire-tables::table.cell>
