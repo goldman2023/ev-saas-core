4b9860f418e8abf8f0160427ba3395f61ba0e2e5
Eimantas <eim.kasp@gmail.com>
2023-01-23 01:50:53 +0200

    Custom Order Page

---
+++ themes/WeShip/views/frontend/page-templates/order-custom.blade.php
@@ -0,0 +1,160 @@
+<div class="w-full flex">
+    <div class="w-[70%]">
+        <img class="h-[100vh] object-cover w-full object-position-right"
+            src="https://businesspress.fra1.digitaloceanspaces.com/uploads/993c7c75-52ff-42ea-9cb6-c149fa874601/1674423379_Emarius34(12).jpg" />
+    </div>
+
+    <div class="w-[30%] p-8">
+        <div class="text-center">
+            <h1 class="text-3xl font-medium mb-3">
+                Emarius 34S
+            </h1>
+            <span>
+                Est. delivery October 15th, 2022
+            </span>
+
+            <div class="text-left mt-6">
+                <span class="font-medium text-lg mb-3 block">
+                    Emarius 35S single engine
+                </span>
+                <fieldset>
+                    <legend class="sr-only">Server size</legend>
+                    <div class="space-y-4">
+                        <!--
+                    Checked: "border-transparent", Not Checked: "border-gray-300"
+                    Active: "border-indigo-500 ring-2 ring-indigo-500"
+                  -->
+                        <label
+                            class="bg-[#DDE2E8] relative block cursor-pointer rounded-full border bg-white px-6 py-4 shadow-sm focus:outline-none sm:flex sm:justify-between">
+                            <input type="radio" name="server-size" value="Hobby" class="sr-only">
+                            <span class="flex items-center">
+                                <span class="flex flex-col text-sm">
+                                    <span id="server-size-0-label" class="font-medium text-gray-900">
+                                        Emarius 34S, 100kwh
+                                    </span>
+                                    <span id="server-size-0-description-0" class="text-gray-500">
+
+                                    </span>
+                                </span>
+                            </span>
+                            <span id="server-size-0-description-1"
+                                class="mt-2 flex text-sm sm:mt-0 sm:ml-4 sm:flex-col sm:text-right">
+                                <span class="font-medium text-gray-900">3528€</span>
+                            </span>
+                            <!--
+                      Active: "border", Not Active: "border-2"
+                      Checked: "border-indigo-500", Not Checked: "border-transparent"
+                    -->
+                            <span class="pointer-events-none absolute -inset-px rounded-full border-2"
+                                aria-hidden="true"></span>
+                        </label>
+
+                        <!--
+                    Checked: "border-transparent", Not Checked: "border-gray-300"
+                    Active: "border-indigo-500 ring-2 ring-indigo-500"
+                  -->
+                        <label
+                            class="relative block cursor-pointer rounded-full border bg-white px-6 py-4 shadow-sm focus:outline-none sm:flex sm:justify-between">
+                            <input type="radio" name="server-size" value="Hobby" class="sr-only">
+                            <span class="flex items-center">
+                                <span class="flex flex-col text-sm">
+                                    <span id="server-size-0-label" class="font-medium text-gray-900">
+                                        Emarius 34S, 150kwh
+                                    </span>
+                                    <span id="server-size-0-description-0" class="text-gray-500">
+
+                                    </span>
+                                </span>
+                            </span>
+                            <span id="server-size-0-description-1"
+                                class="mt-2 flex text-sm sm:mt-0 sm:ml-4 sm:flex-col sm:text-right">
+                                <span class="font-medium text-gray-900">3528€</span>
+                            </span>
+                            <!--
+                      Active: "border", Not Active: "border-2"
+                      Checked: "border-indigo-500", Not Checked: "border-transparent"
+                    -->
+                            <span class="pointer-events-none absolute -inset-px rounded-full border-2"
+                                aria-hidden="true"></span>
+                        </label>
+
+
+                    </div>
+                </fieldset>
+
+                <span class="mt-3 font-medium text-lg mb-3 block">
+                    Diesel generator
+                </span>
+                <fieldset>
+                    <legend class="sr-only">Diesel generator</legend>
+                    <div class="space-y-4">
+                        <!--
+                    Checked: "border-transparent", Not Checked: "border-gray-300"
+                    Active: "border-indigo-500 ring-2 ring-indigo-500"
+                  -->
+                        <label
+                            class="bg-[#DDE2E8] relative block cursor-pointer rounded-full border bg-white px-4 py-3 shadow-sm focus:outline-none sm:flex sm:justify-between">
+                            <input type="radio" name="server-size" value="Hobby" class="sr-only">
+                            <span class="flex items-center">
+                                <span class="flex flex-col text-sm">
+                                    <span id="server-size-0-label" class="font-medium text-gray-900">
+                                        Diesel generator
+                                    </span>
+                                    <span id="server-size-0-description-0" class="text-gray-500">
+
+                                    </span>
+                                </span>
+                            </span>
+                            <span id="server-size-0-description-1"
+                                class="mt-2 flex text-sm sm:mt-0 sm:ml-4 sm:flex-col sm:text-right">
+                            </span>
+                            <!--
+                      Active: "border", Not Active: "border-2"
+                      Checked: "border-indigo-500", Not Checked: "border-transparent"
+                    -->
+                            <span class="pointer-events-none absolute -inset-px rounded-full border-2"
+                                aria-hidden="true"></span>
+                        </label>
+
+                        <!--
+                    Checked: "border-transparent", Not Checked: "border-gray-300"
+                    Active: "border-indigo-500 ring-2 ring-indigo-500"
+                  -->
+                        <label
+                            class="relative block cursor-pointer rounded-full border bg-white  px-4 py-3 shadow-sm focus:outline-none sm:flex sm:justify-between">
+                            <input type="radio" name="server-size" value="Hobby" class="sr-only">
+                            <span class="flex items-center">
+                                <span class="flex flex-col text-sm">
+                                    <span id="server-size-0-label" class="font-medium text-gray-900">
+                                        Without diesel generator
+                                    </span>
+                                    <span id="server-size-0-description-0" class="text-gray-500">
+
+                                    </span>
+                                </span>
+                            </span>
+                            <span id="server-size-0-description-1"
+                                class="mt-2 flex text-sm sm:mt-0 sm:ml-4 sm:flex-col sm:text-right">
+                            </span>
+                            <!--
+                      Active: "border", Not Active: "border-2"
+                      Checked: "border-indigo-500", Not Checked: "border-transparent"
+                    -->
+                            <span class="pointer-events-none absolute -inset-px rounded-full border-2"
+                                aria-hidden="true"></span>
+                        </label>
+
+
+                    </div>
+                </fieldset>
+
+
+
+                <a href="#"
+                    class="mt-6 w-full inline-block rounded-full border-2 border-black bg-white px-6 py-3 text-base font-medium text-black shadow-sm hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 text-center">
+                    Order now
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
