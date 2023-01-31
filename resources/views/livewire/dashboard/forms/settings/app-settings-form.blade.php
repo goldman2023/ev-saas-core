925d9e3606a7f761946f36243dc8fcbb4dc951ee
Eimantas <eim.kasp@gmail.com>
2023-01-31 10:41:07 +0200

    Update app-settings-form.blade.php

--- resources/views/livewire/dashboard/forms/settings/app-settings-form.blade.php
+++ resources/views/livewire/dashboard/forms/settings/app-settings-form.blade.php
@@ -2196,6 +2196,121 @@
                                     </x-system.form-modal>
                                     {{-- END Google Tag Manager --}}
 
+
+                                    {{-- Hubspot --}}
+                                    <li
+                                        class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200 border border-gray-200">
+                                        <div class="flex-1 flex flex-col p-8">
+                                            <img class="devsite-product-logo w-auto" alt="Hubspot logo"
+                                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/HubSpot_Logo.svg/2560px-HubSpot_Logo.svg.png"
+                                                loading="lazy">
+                                            <h3 class="mt-6 text-gray-900 text-sm font-medium">
+                                                {{ translate('Hubspot') }}</h3>
+                                        </div>
+                                        <div>
+                                            <div class="-mt-px flex divide-x divide-gray-200">
+                                                <div class="w-0 flex-1 flex">
+                                                    <div @click="$dispatch('display-modal', {'id': 'app-settings-hubspot'})"
+                                                        class="cursor-pointer relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                                                        @svg('heroicon-o-pencil', ['class' => 'w-5 h-5'])
+                                                        <span class="ml-2">{{ translate('Edit') }}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <x-system.form-modal id="app-settings-hubspot" title="Hubspot">
+                                        <!-- Google Analytics Enabled-->
+                                        <div class="flex flex-col mb-3" x-data="{}">
+                                            <label class="block text-sm font-medium text-gray-900 mb-2">
+                                                {{ translate('Enable Hubspot') }}
+                                            </label>
+
+                                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                                <x-dashboard.form.toggle field="settings.hubspot_enabled" />
+                                            </div>
+                                        </div>
+                                        <!-- END Google Analytics Enabled -->
+
+                                        <!-- Gtag ID-->
+                                        <div class="flex flex-col" x-data="{}">
+                                            <label class="block text-sm font-medium text-gray-900 mb-2">
+                                                {{ translate('Hubspot ID') }}
+                                            </label>
+
+                                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                                <x-dashboard.form.input field="settings.hubspot_id" />
+                                            </div>
+                                        </div>
+                                        <!-- END Gtag ID -->
+
+                                        <div class="w-full flex justify-end mt-4" x-data="{}">
+                                            <button type="button" class="btn btn-primary ml-auto btn-sm" @click="
+                                            $wire.set('settings.hubspot_enabled', settings.hubspot_enabled, true);
+                                        " wire:click="saveIntegrations('integrations.hubspot')">
+                                                {{ translate('Save') }}
+                                            </button>
+                                        </div>
+                                    </x-system.form-modal>
+                                    {{-- END Hubspot --}}
+
+                                    {{-- Hubspot --}}
+                                    <li
+                                        class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200 border border-gray-200">
+                                        <div class="flex-1 flex flex-col p-8">
+                                            <img class="devsite-product-logo w-auto" alt="Dokobit logo"
+                                                src="https://www.dokobit.com/wp-content/uploads/2021/02/Dokobit-logo-media.png"
+                                                loading="lazy">
+                                            <h3 class="mt-6 text-gray-900 text-sm font-medium">
+                                                {{ translate('Dokobit') }}</h3>
+                                        </div>
+                                        <div>
+                                            <div class="-mt-px flex divide-x divide-gray-200">
+                                                <div class="w-0 flex-1 flex">
+                                                    <div @click="$dispatch('display-modal', {'id': 'app-settings-dokobit'})"
+                                                        class="cursor-pointer relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                                                        @svg('heroicon-o-pencil', ['class' => 'w-5 h-5'])
+                                                        <span class="ml-2">{{ translate('Edit') }}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <x-system.form-modal id="app-settings-dokobit" title="Dokobit">
+                                        <!-- Google Analytics Enabled-->
+                                        <div class="flex flex-col mb-3" x-data="{}">
+                                            <label class="block text-sm font-medium text-gray-900 mb-2">
+                                                {{ translate('Enable Dokobit') }}
+                                            </label>
+
+                                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                                <x-dashboard.form.toggle field="settings.dokobit_enabled" />
+                                            </div>
+                                        </div>
+                                        <!-- END Google Analytics Enabled -->
+
+                                        <!-- Gtag ID-->
+                                        <div class="flex flex-col" x-data="{}">
+                                            <label class="block text-sm font-medium text-gray-900 mb-2">
+                                                {{ translate('Dokobit ID') }}
+                                            </label>
+
+                                            <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                                <x-dashboard.form.input field="settings.dokobit_id" />
+                                            </div>
+                                        </div>
+                                        <!-- END Gtag ID -->
+
+                                        <div class="w-full flex justify-end mt-4" x-data="{}">
+                                            <button type="button" class="btn btn-primary ml-auto btn-sm" @click="
+                                            $wire.set('settings.dokobit_enabled', settings.dokobit_enabled, true);
+                                        " wire:click="saveIntegrations('integrations.dokobit')">
+                                                {{ translate('Save') }}
+                                            </button>
+                                        </div>
+                                    </x-system.form-modal>
+                                    {{-- END Dokobit --}}
+
                                     @do_action('view.integrations.end')
                                 </ul>
                             </div>
