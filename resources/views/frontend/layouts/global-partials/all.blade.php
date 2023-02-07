cedf1cdc3bed394cb85b3d07946d98ccf71455d4
Vukasin Jockovic <jockovicvukasin@gmail.com>
2023-02-07 23:49:46 +0000

    WeTailwind scss optimizations + comment tailwind cdn global config view

--- resources/views/frontend/layouts/global-partials/all.blade.php
+++ resources/views/frontend/layouts/global-partials/all.blade.php
@@ -1,3 +1,4 @@
+
 {{-- @include('frontend.layouts.global-partials.global-tailwind-config') --}}
 @include('frontend.layouts.global-partials.global-integrations')
 @include('frontend.layouts.global-partials.global-styles')
