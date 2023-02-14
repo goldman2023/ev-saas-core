78c19b47a7600e4c662e0bbe8843c987f2419aa8
Eimantas <eim.kasp@gmail.com>
2023-02-15 01:20:13 +0200

    Update we-edit-layout.blade.php

--- resources/views/frontend/layouts/we-edit-layout.blade.php
+++ resources/views/frontend/layouts/we-edit-layout.blade.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-@if(\App\Models\Language::where('code', Session::get('locale', Config::get('app.locale')))->first()->rtl == 1)
-    <html dir="rtl" lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
-@else
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
-@endif
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+
 <head>
     {{ seo()->render() }}
 
@@ -18,17 +15,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow">
-    <meta name="description" content="@yield('meta_description', get_setting('meta_description') )"/>
+    <meta name="description" content="@yield('meta_description', get_setting('meta_description') )" />
     <meta name="keywords" content="@yield('meta_keywords', get_setting('meta_keywords') )">
 
     @yield('meta')
 
 
-    <script id="img-proxy-data" type="application/json">@json(\IMG::getIMGProxyData())</script>
+    <script id="img-proxy-data" type="application/json">
+        @json(\IMG::getIMGProxyData())
+    </script>
 
     @if(!isset($detailedProduct) && !isset($customer_product) && !isset($shop) && !isset($page) && !isset($blog))
-        <x-default.system.og-meta>
-        </x-default.system.og-meta>
+    <x-default.system.og-meta>
+    </x-default.system.og-meta>
     @endif
 
 
@@ -37,12 +36,18 @@
     <!-- Theme styles -->
     <link rel="stylesheet" href="{{ \EVS::getThemeStyling() }}">
 
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
     @include('frontend.layouts.global-partials.all')
 
 
     @stack('pre_head_scripts')
 
-    <script src="{{ mix('js/app.js', 'themes/WeTailwind', true, true) }}"></script>
+    <script src="{{ mix('js/alpine.js', 'themes/WeTailwind') }}" defer></script>
+    @themefilexists('js/app.min.js')
+        <script src="{{ mix('js/app.min.js', 'themes/'.\WeTheme::getThemeName()) }}" defer></script>
+    @else
+        <script src="{{ mix('js/app.min.js', 'themes/WeTailwind') }}" defer></script>
+    @endthemefilexists
     <!-- Vendor Scripts -->
 
     <x-default.system.tracking-pixels>
@@ -56,19 +61,18 @@
     <style type="text/css">
         .CodeMirror {
             height: 100% !important;
-            text-align: left;
         }
     </style>
 
-<script src="{{ mix('js/app.js', 'themes/WeTailwind', true, true) }}"></script>
 </head>
+
 <body class="h-full">
     <div class="main-wrapper h-full">
         {{-- Sidebar (Mobile/Tablet and Laptop/Desktop) --}}
-        @include('frontend.dashboard.partials.sidebar')
+        @include('frontend.dashboard.partials.sidebar-small')
         @include('frontend.dashboard.partials.sidebar-mobile')
 
-        <div class="lg:pl-64 flex flex-col">
+        <div class="lg:pl-24 flex flex-col">
             @include('frontend.dashboard.navigation.topbar')
 
             <main class="flex-1">
@@ -90,4 +94,5 @@
 
     @stack('footer_scripts')
 </body>
+
 </html>
