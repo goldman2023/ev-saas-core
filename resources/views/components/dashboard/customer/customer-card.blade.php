142b1d100b8e4d35622d46755a29ee045c65990d
Eimantas <eim.kasp@gmail.com>
2023-02-22 06:02:54 +0200

    Refactor notification modal component

--- resources/views/components/dashboard/customer/customer-card.blade.php
+++ resources/views/components/dashboard/customer/customer-card.blade.php
@@ -80,14 +80,18 @@
         </div>
     @else
         @admin
-            <div>
+            <div x-data="{
+                add() {
+                    $dispatch('display-modal', {'id': 'notification-modal'})
+                }
+            }">
                 <h3 class="mb-2 text-base font-bold text-gray-900">
                     {{ translate('Actions') }}
                 </h3>
                 <div class="flex space-x-3">
                     <div class="w-full grid grid-cols-1 gap-3">
                         <button type="button"
-                        data-modal-target="notification-modal" data-modal-toggle="notification-modal"
+                        @click="add()"
                             class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2">
                             <svg class="-ml-1 mr-2 h-5 w-5 text-gray-400" x-description="Heroicon name: mini/envelope"
                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
