27df628fe0d0efcb79877a05d4ee960160edf361
Eimantas <eim.kasp@gmail.com>
2023-03-02 02:49:58 +0200

    [WeBaltic] - Include order steps in create form

---
+++ themes/WeBaltic/views/frontend/dashboard/orders/create.blade.php
@@ -0,0 +1,29 @@
+@extends('frontend.layouts.user_panel')
+
+@section('page_title', translate('Create New Order'))
+
+@push('head_scripts')
+
+@endpush
+
+@section('panel_content')
+    <section>
+        <x-dashboard.section-headers.section-header title="{{ translate('New Order') }}" text="">
+            <x-slot name="content">
+                <a href="{{ route('orders.index') }}" class="btn-standard">
+                    @svg('heroicon-o-chevron-left', ['class' => 'h-4 h-4 mr-2'])
+                    <span>{{ translate('All orders') }}</span>
+                </a>
+            </x-slot>
+        </x-dashboard.section-headers.section-header>
+
+        <x-theme::dashboard.orders.order-steps></x-theme::dashboard.orders.order-steps>
+
+        <livewire:dashboard.forms.orders.order-form :customer="$customer" hide-shipping="true" />
+    </section>
+@endsection
+
+@push('footer_scripts')
+
+@endpush
+
